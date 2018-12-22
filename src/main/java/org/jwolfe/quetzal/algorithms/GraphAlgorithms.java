cf52e7362e374875710c7fe5e2c8626ede49df9a
John Wolfe <dr.joewolfe@gmail.com>
2018-12-23 01:28:18 +0530

    +1 to graph-algorithms.

--- src/main/java/org/jwolfe/quetzal/algorithms/GraphAlgorithms.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/GraphAlgorithms.java
@@ -1,135 +1,196 @@
 package org.jwolfe.quetzal.algorithms;
 
 import org.jwolfe.quetzal.library.graph.AdjacencyGraph;
+import org.jwolfe.quetzal.library.utilities.Utilities;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class GraphAlgorithms {
-    public static int[] dijkstrasShortestPaths(int[][] graph, int numVertices, int startVertex) {
-        int[] distances = new int[numVertices];
-        boolean[] sptSet = new boolean[numVertices];
+	public static int[] dijkstrasShortestPaths(int[][] graph, int numVertices, int startVertex) {
+		int[] distances = new int[numVertices];
+		boolean[] sptSet = new boolean[numVertices];
 
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        Arrays.fill(sptSet, false);
+		Arrays.fill(distances, Integer.MAX_VALUE);
+		Arrays.fill(sptSet, false);
 
-        distances[startVertex] = 0;
+		distances[startVertex] = 0;
 
-        for (int i = 0; i < numVertices; i++) {
-            int u = getShortestDistanceVertexForSPTSet(distances, sptSet);
+		for (int i = 0; i < numVertices; i++) {
+			int u = getShortestDistanceVertexForSPTSet(distances, sptSet);
 
-            sptSet[u] = true;
-            for(int v = 0; v < numVertices; v++) {
-                if (!sptSet[v]
-                        && graph[u][v] != 0
-                        && distances[u] != Integer.MAX_VALUE
-                        && distances[u] + graph[u][v] < distances[v]) {
-                    distances[v] = distances[u] + graph[u][v];
-                }
-            }
-        }
+			sptSet[u] = true;
+			for (int v = 0; v < numVertices; v++) {
+				if (!sptSet[v] && graph[u][v] != 0 && distances[u] != Integer.MAX_VALUE
+						&& distances[u] + graph[u][v] < distances[v]) {
+					distances[v] = distances[u] + graph[u][v];
+				}
+			}
+		}
 
-        return distances;
-    }
+		return distances;
+	}
 
-    private static int getShortestDistanceVertexForSPTSet(int[] distances, boolean[] sptSet) {
-        int minValue = Integer.MAX_VALUE;
-        int minIndex = -1;
+	public static int[] dialsShortestPaths(int[][] graph, int numVertices, int startVertex) {
+		if (graph == null || graph.length > numVertices || startVertex >= numVertices) {
+			return null;
+		}
 
-        for (int i = 0; i < sptSet.length; i++) {
-            if (!sptSet[i]) {
-                if (distances[i] <= minValue) {
-                    minValue = distances[i];
-                    minIndex = i;
-                }
-            }
-        }
+		int maxWeight = Utilities.getMaxValue(graph);
+		int numBuckets = maxWeight * numVertices + 1;
 
-        return minIndex;
-    }
+		int[] distances = new int[numVertices];
+		List<LinkedList<Integer>> buckets = new ArrayList<>();
 
-    public static void primMST(int[][] graph) {
-        int vertices = graph.length;
-        int[] parent = new int[vertices];
-        int[] keys = new int[vertices];
-        boolean[] inMst = new boolean[vertices];
+		Arrays.fill(distances, Integer.MAX_VALUE);
+		for (int i = 0; i < numBuckets; i++) {
+			buckets.add(new LinkedList<>());
+		}
 
-        Arrays.fill(keys, Integer.MAX_VALUE);
-        Arrays.fill(inMst, false);
+		distances[startVertex] = 0;
+		buckets.get(0).offer(startVertex);
 
-        keys[0] = 0;
-        parent[0] = -1;
+		int bucketIndex = 0;
+		while (true) {
+			// Find first non-empty bucket
+			for (bucketIndex = 0; bucketIndex < numBuckets; bucketIndex++) {
+				if (buckets.get(bucketIndex).size() > 0) {
+					break;
+				}
+			}
 
-        for(int i = 0; i < vertices; i++) {
-            int u = getMinIndexFromKeys(keys, inMst);
-            inMst[u] = true;
+			if (bucketIndex == numBuckets) {
+				// All buckets are empty
+				break;
+			}
 
-            for(int v = 0; v < vertices; v++) {
-                if(graph[u][v] != 0
-                        && inMst[v] == false
-                        && graph[u][v] < keys[v]) {
-                    parent[v] = u;
-                    keys[v] = graph[u][v];
-                }
-            }
-        }
+			var u = buckets.get(bucketIndex).poll();
+			for (Integer v = 0; v < numVertices; v++) {
+				if (u == v || graph[u][v] == 0) {
+					continue;
+				}
 
-        for(int i = 1; i < vertices; i++) {
-            System.out.println((i) + ":\t" + parent[i] + "\t-\t" + i + "\t(" + graph[i][parent[i]] + ")");
-        }
-    }
+				// v is adjacent to u
+				int w = graph[u][v];
 
-    private static int getMinIndexFromKeys(int[] keys, boolean[] inMst) {
-        int minKey = Integer.MAX_VALUE;
-        int minIndex = -1;
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i] < minKey
-                    && inMst[i] == false) {
-                minKey = keys[i];
-                minIndex = i;
-            }
-        }
+				int du = distances[u];
+				int dv = distances[v];
 
-        return minIndex;
-    }
+				if (du + w < dv) {
+					if (dv != Integer.MAX_VALUE) {
+						// Remove v from its old bucket
+						buckets.get(dv).remove(v);
+					}
 
-    public static int[] topologicalSort(AdjacencyGraph graph) {
-        // Works only for a Directed Acyclic Graph
+					// Add v to the new weight bucket
+					dv = du + w;
+					distances[v] = dv;
+					buckets.get(dv).add(v);
+				}
+			}
+		}
 
-        if(graph == null) {
-            return null;
-        }
+		return distances;
+	}
 
-        boolean[] visited = new boolean[graph.getVertexCount()];
-        Arrays.fill(visited, false);
+	private static int getShortestDistanceVertexForSPTSet(int[] distances, boolean[] sptSet) {
+		int minValue = Integer.MAX_VALUE;
+		int minIndex = -1;
 
-        Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < sptSet.length; i++) {
+			if (!sptSet[i]) {
+				if (distances[i] <= minValue) {
+					minValue = distances[i];
+					minIndex = i;
+				}
+			}
+		}
 
-        for (int u = 0; u < graph.getVertexCount(); u++) {
-            if(!visited[u]) {
-                topologicalSort(graph, u, visited, stack);
-            }
-        }
+		return minIndex;
+	}
 
-        int[] sortedTopology = new int[graph.getVertexCount()];
-        int index = 0;
-        while(!stack.isEmpty()) {
-            sortedTopology[index++] = stack.pop();
-        }
+	public static void primMST(int[][] graph) {
+		int vertices = graph.length;
+		int[] parent = new int[vertices];
+		int[] keys = new int[vertices];
+		boolean[] inMst = new boolean[vertices];
 
-        return sortedTopology;
-    }
+		Arrays.fill(keys, Integer.MAX_VALUE);
+		Arrays.fill(inMst, false);
 
-    private static void topologicalSort(AdjacencyGraph graph, int u, boolean[] visited, Stack<Integer> stack) {
-        visited[u] = true;
+		keys[0] = 0;
+		parent[0] = -1;
 
-        for(var v : graph.getAdjacencyList()[u]) {
-            if(!visited[v]) {
-                topologicalSort(graph, v, visited, stack);
-            }
-        }
+		for (int i = 0; i < vertices; i++) {
+			int u = getMinIndexFromKeys(keys, inMst);
+			inMst[u] = true;
 
-        stack.push(u);
-    }
+			for (int v = 0; v < vertices; v++) {
+				if (graph[u][v] != 0 && inMst[v] == false && graph[u][v] < keys[v]) {
+					parent[v] = u;
+					keys[v] = graph[u][v];
+				}
+			}
+		}
+
+		for (int i = 1; i < vertices; i++) {
+			System.out.println((i) + ":\t" + parent[i] + "\t-\t" + i + "\t(" + graph[i][parent[i]] + ")");
+		}
+	}
+
+	private static int getMinIndexFromKeys(int[] keys, boolean[] inMst) {
+		int minKey = Integer.MAX_VALUE;
+		int minIndex = -1;
+		for (int i = 0; i < keys.length; i++) {
+			if (keys[i] < minKey && inMst[i] == false) {
+				minKey = keys[i];
+				minIndex = i;
+			}
+		}
+
+		return minIndex;
+	}
+
+	public static int[] topologicalSort(AdjacencyGraph graph) {
+		// Works only for a Directed Acyclic Graph
+
+		if (graph == null) {
+			return null;
+		}
+
+		boolean[] visited = new boolean[graph.getVertexCount()];
+		Arrays.fill(visited, false);
+
+		Stack<Integer> stack = new Stack<>();
+
+		for (int u = 0; u < graph.getVertexCount(); u++) {
+			if (!visited[u]) {
+				topologicalSort(graph, u, visited, stack);
+			}
+		}
+
+		int[] sortedTopology = new int[graph.getVertexCount()];
+		int index = 0;
+		while (!stack.isEmpty()) {
+			sortedTopology[index++] = stack.pop();
+		}
+
+		return sortedTopology;
+	}
+
+	private static void topologicalSort(AdjacencyGraph graph, int u, boolean[] visited, Stack<Integer> stack) {
+		visited[u] = true;
+
+		for (var v : graph.getAdjacencyList()[u]) {
+			if (!visited[v]) {
+				topologicalSort(graph, v, visited, stack);
+			}
+		}
+
+		stack.push(u);
+	}
 }
-
--- src/test/java/org/jwolfe/quetzal/algorithms/GraphAlgorithmsTest.java
+++ src/test/java/org/jwolfe/quetzal/algorithms/GraphAlgorithmsTest.java
@@ -24,7 +24,29 @@ class GraphAlgorithmsTest {
         };
 
         var distances = GraphAlgorithms.dijkstrasShortestPaths(graph, 9, 0);
-        System.out.println(Arrays.toString(distances));
+        var expectedDistances = Utilities.constructArray(0, 4, 12, 19, 21, 11, 9, 8, 14);
+        Utilities.printArray(distances);
+        assertArrayEquals(expectedDistances, distances);
+    }
+    
+    @Test
+    void dialsShortestPaths() {
+        int[][] graph = new int[][]{
+                {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                {0, 0, 2, 0, 0, 0, 6, 7, 0}
+        };
+        
+        var distances = GraphAlgorithms.dialsShortestPaths(graph, 9, 0);
+        var expectedDistances = Utilities.constructArray(0, 4, 12, 19, 21, 11, 9, 8, 14);
+        Utilities.printArray(distances);
+        assertArrayEquals(expectedDistances, distances);
     }
 
     @Test
