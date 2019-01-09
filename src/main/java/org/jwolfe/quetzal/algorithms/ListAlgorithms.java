d48cd1b46f0dfc82d1af29978a2f9f776c0cb1b3
John Wolfe <dr.joewolfe@gmail.com>
2019-01-09 23:32:42 +0530

    +1 to list-algorithms (get-all-subsets, backtracking)

--- src/main/java/org/jwolfe/quetzal/algorithms/ListAlgorithms.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/ListAlgorithms.java
@@ -1,8 +1,6 @@
 package org.jwolfe.quetzal.algorithms;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
+import java.util.*;
 
 import org.jwolfe.quetzal.library.utilities.Utilities;
 
@@ -56,7 +54,37 @@ public class ListAlgorithms {
 	public static <T> List<List<T>> getAllSubsetsSorted(List<T> list) {
 		var allSubsets = getAllSubsets(list);
 		Collections.sort(allSubsets, (x, y) -> x.size() - y.size());
-		
+
 		return allSubsets;
+	}
+
+	public static <T> Set<Set<T>> getAllSubsets(Set<T> set) {
+		// Backtracking based implementation for get all subsets
+		if (set == null) {
+			return null;
+		}
+
+		Set<Set<T>> subsets = new HashSet<>();
+		// Add the empty subset
+		subsets.add(new HashSet<>());
+
+		List<T> list = new ArrayList<>();
+		list.addAll(set);
+
+		Set<T> currentSet = new HashSet<>();
+		getAllSubsets(list, list.size(), 0, currentSet, subsets);
+
+		return subsets;
+	}
+
+	private static <T> void getAllSubsets(List<T> list, int length, int index, Set<T> currentSet, Set<Set<T>> subsets) {
+		for (int i = index; i < length; i++) {
+			T currentElement = list.get(i);
+			currentSet.add(currentElement);
+			subsets.add(new TreeSet<>(currentSet));
+			getAllSubsets(list, length, i + 1, currentSet, subsets);
+
+			currentSet.remove(currentElement);
+		}
 	}
 }
--- src/test/java/org/jwolfe/quetzal/algorithms/ListAlgorithmsTest.java
+++ src/test/java/org/jwolfe/quetzal/algorithms/ListAlgorithmsTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.junit.jupiter.api.Test;
 import org.jwolfe.quetzal.library.utilities.Utilities;
+import org.jwolfe.quetzal.test.QuetzalAssertions;
 
 class ListAlgorithmsTest {
 
@@ -23,11 +26,32 @@ class ListAlgorithmsTest {
 		var allSubsets = ListAlgorithms.getAllSubsets(list);
 		Utilities.printListByLines(allSubsets);
 	}
-	
+
 	@Test
 	void getAllSubsetsSorted() {
 		List<Integer> list = Utilities.createArrayList(1, 2, 3);
 		var allSubsets = ListAlgorithms.getAllSubsetsSorted(list);
 		Utilities.printListByLines(allSubsets);
+	}
+
+	@Test
+	void getAllSubSets() {
+		Set<Integer> set;
+		Set<Set<Integer>> subsets;
+		Set<Set<Integer>> expectedSubsets;
+
+		set = Utilities.constructSet(1, 2, 3);
+		expectedSubsets = Utilities.constructSet(
+				new TreeSet<>(),
+				Utilities.constructSet(1),
+				Utilities.constructSet(1, 2),
+				Utilities.constructSet(1, 2, 3),
+				Utilities.constructSet(1, 3),
+				Utilities.constructSet(2),
+				Utilities.constructSet(2, 3),
+				Utilities.constructSet(3));
+		subsets = ListAlgorithms.getAllSubsets(set);
+		QuetzalAssertions.assertSetEquals(expectedSubsets, subsets);
+		Utilities.printSetByLines(subsets);
 	}
 }
