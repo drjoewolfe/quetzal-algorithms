3b8185ece8edcc7c12a829e7fab4264c5516d069
Johan Wolfe <drjoewolfe@gmail.com>
2021-04-06 15:23:03 +0530

    1551. Minimum Operations to Make Array Equal

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumOperationsToMakeArrayEqual.java
@@ -0,0 +1,51 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class MinimumOperationsToMakeArrayEqual {
+    class Solution {
+        public int minOperations(int n) {
+            if(n < 2) {
+                return 0;
+            }
+
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++) {
+                arr[i] = (2 * i) + 1;
+            }
+
+            int target = (n % 2 != 0) ? arr[n / 2] : (arr[n / 2 - 1]  + arr[n / 2]) / 2;
+            int operations = 0;
+            for(int i = 0; i < n; i++) {
+                operations += Math.abs(arr[i] - target);
+            }
+
+            return operations / 2;
+        }
+    }
+}
+
+//    1551. Minimum Operations to Make Array Equal
+//    Medium
+//    You have an array arr of length n where arr[i] = (2 * i) + 1 for all valid values of i (i.e. 0 <= i < n).
+//
+//    In one operation, you can select two indices x and y where 0 <= x, y < n and subtract 1 from arr[x] and add 1 to arr[y] (i.e. perform arr[x] -=1 and arr[y] += 1). The goal is to make all the elements of the array equal. It is guaranteed that all the elements of the array can be made equal using some operations.
+//
+//    Given an integer n, the length of the array. Return the minimum number of operations needed to make all the elements of arr equal.
+//
+//
+//
+//    Example 1:
+//
+//    Input: n = 3
+//    Output: 2
+//    Explanation: arr = [1, 3, 5]
+//    First operation choose x = 2 and y = 0, this leads arr to be [2, 3, 4]
+//    In the second operation choose x = 2 and y = 0 again, thus arr = [3, 3, 3].
+//    Example 2:
+//
+//    Input: n = 6
+//    Output: 9
+//
+//
+//    Constraints:
+//
+//    1 <= n <= 10^4
