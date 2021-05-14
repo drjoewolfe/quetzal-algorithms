84e473b234dc612d18c459ae48a66fa2f416faf9
Johan Wolfe <drjoewolfe@gmail.com>
2021-05-14 14:00:29 +0530

    114. Flatten Binary Tree to Linked List

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,86 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.Stack;
+
+public class FlattenBinaryTreeToLinkedList {
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+
+         TreeNode() {
+         }
+
+         TreeNode(int val) {
+             this.val = val;
+         }
+
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+
+    class Solution {
+        public void flatten(TreeNode root) {
+            if(root == null) {
+                return;
+            }
+
+            Stack<TreeNode> stack = new Stack<>();
+            stack.push(root);
+
+            TreeNode prev = null;
+            while(!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+
+                if(prev != null) {
+                    prev.left = null;
+                    prev.right = node;
+                }
+
+                if(node.right != null) {
+                    stack.push(node.right);
+                }
+
+                if(node.left != null) {
+                    stack.push(node.left);
+                }
+
+                prev = node;
+            }
+        }
+    }
+}
+
+//    114. Flatten Binary Tree to Linked List
+//    Medium
+//    Given the root of a binary tree, flatten the tree into a "linked list":
+//
+//    The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+//    The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+//
+//
+//    Example 1:
+//
+//
+//    Input: root = [1,2,5,3,4,null,6]
+//    Output: [1,null,2,null,3,null,4,null,5,null,6]
+//    Example 2:
+//
+//    Input: root = []
+//    Output: []
+//    Example 3:
+//
+//    Input: root = [0]
+//    Output: [0]
+//
+//
+//    Constraints:
+//
+//    The number of nodes in the tree is in the range [0, 2000].
+//    -100 <= Node.val <= 100
+//
+//
+//    Follow up: Can you flatten the tree in-place (with O(1) extra space)?
