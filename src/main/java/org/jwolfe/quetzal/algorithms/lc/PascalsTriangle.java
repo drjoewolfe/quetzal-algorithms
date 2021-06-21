eaa7a90b6115b25cba6a774499e4c34e6e5f7435
Johan Wolfe <dr.joewolfe@gmail.com>
2021-06-21 14:43:37 +0530

    118. Pascal's Triangle

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/PascalsTriangle.java
@@ -0,0 +1,60 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> triangle = new ArrayList<>();
+
+            if(numRows < 1) {
+                return triangle;
+            }
+
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+
+            triangle.add(row);
+
+            for(int i = 2; i <= numRows; i++) {
+                List<Integer> previousRow = row;
+                row = new ArrayList<>();
+
+                row.add(1);
+                for(int j = 1; j < previousRow.size(); j++) {
+                    row.add(previousRow.get(j - 1) + previousRow.get(j));
+                }
+
+                row.add(1);
+
+                triangle.add(row);
+            }
+
+            return triangle;
+        }
+    }
+}
+
+//    118. Pascal's Triangle
+//    Easy
+//    Given an integer numRows, return the first numRows of Pascal's triangle.
+//
+//    In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+//
+//
+//
+//
+//    Example 1:
+//
+//    Input: numRows = 5
+//    Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+//    Example 2:
+//
+//    Input: numRows = 1
+//    Output: [[1]]
+//
+//
+//    Constraints:
+//
+//    1 <= numRows <= 30
