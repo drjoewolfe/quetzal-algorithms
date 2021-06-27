07b36586ae26a5fa9ef08626df7f1d04a9761ef1
Johan Wolfe <dr.joewolfe@gmail.com>
2021-06-28 02:59:56 +0530

    14. Longest Common Prefix

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/LongestCommonPrefix.java
@@ -0,0 +1,82 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            if (strs == null || strs.length == 0) {
+                return "";
+            }
+
+            String prefix = strs[0];
+            for (int i = 1; i < strs.length; i++) {
+                while (!strs[i].startsWith(prefix)) {
+                    prefix = prefix.substring(0, prefix.length() - 1);
+
+                    if (prefix.isEmpty()) {
+                        return "";
+                    }
+                }
+            }
+
+            return prefix;
+        }
+    }
+
+    class Solution_Brute {
+        public String longestCommonPrefix(String[] strs) {
+            if (strs == null || strs.length == 0) {
+                return "";
+            }
+
+            if (strs.length == 1) {
+                return strs[0];
+            }
+
+            Arrays.sort(strs);
+
+            String firstString = strs[0];
+            int index = 0;
+
+            for (; index < firstString.length(); index++) {
+                char c = firstString.charAt(index);
+
+                for (int j = 1; j < strs.length; j++) {
+                    String nextString = strs[j];
+
+                    if (nextString.length() <= index || nextString.charAt(index) != c) {
+                        return firstString.substring(0, index);
+                    }
+                }
+            }
+
+            return firstString.substring(0, index);
+        }
+    }
+}
+
+//    14. Longest Common Prefix
+//    Easy
+//    Write a function to find the longest common prefix string amongst an array of strings.
+//
+//    If there is no common prefix, return an empty string "".
+//
+//
+//
+//    Example 1:
+//
+//    Input: strs = ["flower","flow","flight"]
+//    Output: "fl"
+//    Example 2:
+//
+//    Input: strs = ["dog","racecar","car"]
+//    Output: ""
+//    Explanation: There is no common prefix among the input strings.
+//
+//
+//    Constraints:
+//
+//    1 <= strs.length <= 200
+//    0 <= strs[i].length <= 200
+//    strs[i] consists of only lower-case English letters.
