4b2e88481253fa79cdb88ac0c0db403b1f52ff30
Johan Wolfe <dr.joewolfe@gmail.com>
2021-06-30 20:34:09 +0530

    236. Lowest Common Ancestor of a Binary Tree

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,90 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class LowestCommonAncestorOfABinaryTree {
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root == null) {
+                return null;
+            }
+
+            if(root.val == p.val || root.val == q.val) {
+                return root;
+            }
+
+            TreeNode leftResult = lowestCommonAncestor(root.left, p, q);
+            // Optimization
+            if(leftResult != null && leftResult.val != p.val && leftResult.val != q.val) {
+                return leftResult;
+            }
+
+            TreeNode rightResult = lowestCommonAncestor(root.right, p, q);
+
+            if(leftResult != null && rightResult != null) {
+                return root;
+            }
+
+            return leftResult != null ? leftResult : rightResult;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root == null) {
+                return null;
+            }
+
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+            if(left != null && right != null) {
+                // Current node is the LCA
+                return root;
+            } else if(root == p ||root == q) {
+                return root;
+            } else {
+                return left != null ? left : right;
+            }
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
+
+//    236. Lowest Common Ancestor of a Binary Tree
+//    Medium
+//    Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+//
+//    According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+//
+//
+//
+//    Example 1:
+//
+//
+//    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//    Output: 3
+//    Explanation: The LCA of nodes 5 and 1 is 3.
+//    Example 2:
+//
+//
+//    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//    Output: 5
+//    Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+//    Example 3:
+//
+//    Input: root = [1,2], p = 1, q = 2
+//    Output: 1
+//
+//
+//    Constraints:
+//
+//    The number of nodes in the tree is in the range [2, 105].
+//    -109 <= Node.val <= 109
+//    All Node.val are unique.
+//    p != q
+//    p and q will exist in the tree.
