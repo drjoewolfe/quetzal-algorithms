4134967a489bcb6016971077103b7e820dafce6e
Johan Wolfe <dr.joewolfe@gmail.com>
2021-08-01 02:37:52 +0530

    42. Trapping Rain Water

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/TrappingRainWater.java
@@ -0,0 +1,89 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class TrappingRainWater {
+    class Solution {
+        public int trap(int[] height) {
+            if(height == null || height.length < 3) {
+                return 0;
+            }
+
+            int length = height.length;
+
+            int[] largestToRight = new int[length];
+            largestToRight[length - 1] = height[length - 1];
+            for(int i = length - 2; i >=0; i--) {
+                largestToRight[i] = Math.max(largestToRight[i + 1], height[i]);
+            }
+
+            int volume = 0;
+            int left = 0;
+            int right = 0;
+            int current = 0;
+            for(int i = 1; i < height.length - 1; i++) {
+                left = Math.max(left, height[i - 1]);
+                right = largestToRight[i];
+                current = height[i];
+
+                int currentVolume = Math.min(left, right) - current;
+                volume += Math.max(currentVolume, 0);
+            }
+
+            return volume;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public int trap(int[] height) {
+            if(height == null || height.length < 2) {
+                return 0;
+            }
+
+            int n = height.length;
+            int[] maxFromRight = new int[n];
+            maxFromRight[n - 1] = height[n - 1];
+            for(int i = n - 2; i >= 0; i--) {
+                maxFromRight[i] = Math.max(height[i], maxFromRight[i + 1]);
+            }
+
+            int trappedWater = 0;
+            int maxFromLeft = height[0];
+            for(int i = 1; i < n - 1; i++) {
+                trappedWater += Math.max(
+                        Math.min(maxFromLeft, maxFromRight[i + 1]) - height[i],
+                        0);
+
+                maxFromLeft = Math.max(maxFromLeft, height[i]);
+            }
+
+            return trappedWater;
+        }
+    }
+
+// Testcases
+//  [0,1,0,2,1,0,1,3,2,1,2,1]
+//  [2,0,2]
+}
+
+//    42. Trapping Rain Water
+//    Hard
+//    Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+//
+//
+//
+//    Example 1:
+//
+//
+//    Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+//    Output: 6
+//    Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+//    Example 2:
+//
+//    Input: height = [4,2,0,3,2,5]
+//    Output: 9
+//
+//
+//    Constraints:
+//
+//    n == height.length
+//    0 <= n <= 3 * 104
+//    0 <= height[i] <= 105
