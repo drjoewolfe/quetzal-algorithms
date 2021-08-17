79de1cd57ad5493629660e409430c175ea53fc95
Johan Wolfe <dr.joewolfe@gmail.com>
2021-08-17 18:19:52 +0530

    1448. Count Good Nodes in Binary Tree

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CountGoodNodesInBinaryTree.java
@@ -0,0 +1,135 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountGoodNodesInBinaryTree {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        int count;
+        public int goodNodes(TreeNode root) {
+            count = 0;
+            goodNodes(root, Integer.MIN_VALUE);
+
+            return count;
+        }
+
+        private void goodNodes(TreeNode root, int maxValueInPath) {
+            if(root == null) {
+                return;
+            }
+
+            if(root.val >= maxValueInPath) {
+                maxValueInPath = root.val;
+                count++;
+            }
+
+            goodNodes(root.left, maxValueInPath);
+            goodNodes(root.right, maxValueInPath);
+        }
+    }
+
+    class Solution_Correct_2 {
+        int goodNodeCount;
+
+        public int goodNodes(TreeNode root) {
+            if(root == null) {
+                return 0;
+            }
+
+            List<TreeNode> path = new ArrayList<>();
+            goodNodeCount = 0;
+            goodNodes(root, path);
+
+            return goodNodeCount;
+        }
+
+        private void goodNodes(TreeNode root, List<TreeNode> path) {
+            if(root == null) {
+                return;
+            }
+
+            boolean isGoodNode = true;
+            for(TreeNode node : path) {
+                if(node.val > root.val) {
+                    isGoodNode = false;
+                    break;
+                }
+            }
+
+            if(isGoodNode) {
+                goodNodeCount++;
+            }
+
+            path.add(root);
+            goodNodes(root.left, path);
+            goodNodes(root.right, path);
+            path.remove(path.size() - 1);
+        }
+    }
+
+     public class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+}
+
+//    1448. Count Good Nodes in Binary Tree
+//    Medium
+//    Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+//
+//    Return the number of good nodes in the binary tree.
+//
+//
+//
+//    Example 1:
+//
+//
+//
+//    Input: root = [3,1,4,3,null,1,5]
+//    Output: 4
+//    Explanation: Nodes in blue are good.
+//    Root Node (3) is always a good node.
+//    Node 4 -> (3,4) is the maximum value in the path starting from the root.
+//    Node 5 -> (3,4,5) is the maximum value in the path
+//    Node 3 -> (3,1,3) is the maximum value in the path.
+//    Example 2:
+//
+//
+//
+//    Input: root = [3,3,null,4,2]
+//    Output: 3
+//    Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+//    Example 3:
+//
+//    Input: root = [1]
+//    Output: 1
+//    Explanation: Root is considered as good.
+//
+//
+//    Constraints:
+//
+//    The number of nodes in the binary tree is in the range [1, 10^5].
+//    Each node's value is between [-10^4, 10^4].
