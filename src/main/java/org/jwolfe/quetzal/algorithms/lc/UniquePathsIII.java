de008ba5ac535924afa48f5047d762a7b9cff8f6
Johan Wolfe <dr.joewolfe@gmail.com>
2021-11-02 16:06:45 +0530

    980. Unique Paths III

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/UniquePathsIII.java
@@ -0,0 +1,132 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class UniquePathsIII {
+    class Solution {
+        int uniquePathCount;
+
+        public int uniquePathsIII(int[][] grid) {
+            if(grid == null || grid.length == 0 || grid[0].length == 0) {
+                return 0;
+            }
+
+            uniquePathCount = 0;
+
+            GridMetadata metadata = computeMetadata(grid);
+            boolean[][] visited = new boolean[metadata.rowCount][metadata.colCount];
+
+            computeUniquePathCount(grid, metadata.startRow, metadata.startCol, metadata, visited, 0);
+
+            return uniquePathCount;
+        }
+
+        private GridMetadata computeMetadata(int[][] grid) {
+            GridMetadata metadata = new GridMetadata();
+            metadata.rowCount = grid.length;
+            metadata.colCount = grid[0].length;
+
+            for(int r = 0; r < grid.length; r++) {
+                for(int c = 0; c < grid[0].length; c++) {
+                    int val = grid[r][c];
+
+                    if(val == 1) {
+                        metadata.startRow = r;
+                        metadata.startCol = c;
+                    } else if(val == 2) {
+                        metadata.endRow = r;
+                        metadata.endCol = c;
+                    } else if(val == 0) {
+                        metadata.emptyCellCount++;
+                    }
+                }
+            }
+
+            return metadata;
+        }
+
+        private void computeUniquePathCount(int[][] grid, int row, int col, GridMetadata metadata, boolean[][] visited, int currentCount) {
+            if(row < 0 || row >= metadata.rowCount || col < 0 || col >= metadata.colCount || visited[row][col]
+                    || grid[row][col] == -1) {
+                return;
+            }
+
+            if(grid[row][col] == 2) {
+                if(currentCount == metadata.emptyCellCount) {
+                    uniquePathCount++;
+                }
+
+                return;
+            }
+
+            currentCount = grid[row][col] != 1 ? currentCount + 1 : currentCount;
+            visited[row][col] = true;
+
+            computeUniquePathCount(grid, row + 1, col, metadata, visited, currentCount);
+            computeUniquePathCount(grid, row - 1, col, metadata, visited, currentCount);
+            computeUniquePathCount(grid, row, col + 1, metadata, visited, currentCount);
+            computeUniquePathCount(grid, row, col - 1, metadata, visited, currentCount);
+
+            visited[row][col] = false;
+        }
+
+        private class GridMetadata {
+            int rowCount;
+            int colCount;
+
+            int startRow;
+            int startCol;
+
+            int endRow;
+            int endCol;
+
+            int emptyCellCount;
+        }
+    }
+}
+
+//    980. Unique Paths III
+//    Hard
+//    You are given an m x n integer array grid where grid[i][j] could be:
+//
+//    1 representing the starting square. There is exactly one starting square.
+//    2 representing the ending square. There is exactly one ending square.
+//    0 representing empty squares we can walk over.
+//    -1 representing obstacles that we cannot walk over.
+//    Return the number of 4-directional walks from the starting square to the ending square, that walk over every non-obstacle square exactly once.
+//
+//
+//
+//    Example 1:
+//
+//
+//    Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,2,-1]]
+//    Output: 2
+//    Explanation: We have the following two paths:
+//    1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2)
+//    2. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2)
+//    Example 2:
+//
+//
+//    Input: grid = [[1,0,0,0],[0,0,0,0],[0,0,0,2]]
+//    Output: 4
+//    Explanation: We have the following four paths:
+//    1. (0,0),(0,1),(0,2),(0,3),(1,3),(1,2),(1,1),(1,0),(2,0),(2,1),(2,2),(2,3)
+//    2. (0,0),(0,1),(1,1),(1,0),(2,0),(2,1),(2,2),(1,2),(0,2),(0,3),(1,3),(2,3)
+//    3. (0,0),(1,0),(2,0),(2,1),(2,2),(1,2),(1,1),(0,1),(0,2),(0,3),(1,3),(2,3)
+//    4. (0,0),(1,0),(2,0),(2,1),(1,1),(0,1),(0,2),(0,3),(1,3),(1,2),(2,2),(2,3)
+//    Example 3:
+//
+//
+//    Input: grid = [[0,1],[2,0]]
+//    Output: 0
+//    Explanation: There is no path that walks over every empty square exactly once.
+//    Note that the starting and ending square can be anywhere in the grid.
+//
+//
+//    Constraints:
+//
+//    m == grid.length
+//    n == grid[i].length
+//    1 <= m, n <= 20
+//    1 <= m * n <= 20
+//    -1 <= grid[i][j] <= 2
+//    There is exactly one starting cell and one ending cell.
