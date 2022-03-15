fd9eb62c3c251409019675cc0f181851f0a230c6
Johan Wolfe <dr.joewolfe@gmail.com>
2022-03-16 04:48:46 +0530

    1249. Minimum Remove to Make Valid Parentheses

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumRemoveToMakeValidParentheses.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumRemoveToMakeValidParentheses.java
@@ -5,40 +5,81 @@ import java.util.Set;
 import java.util.Stack;
 
 public class MinimumRemoveToMakeValidParentheses {
-    public String minRemoveToMakeValid(String s) {
-        if(s == null || s.length() == 0) {
-            return null;
-        }
+    class Solution {
+        public String minRemoveToMakeValid(String s) {
+            if(s == null || s.length() == 0) {
+                return "";
+            }
 
-        Set<Integer> indicesToRemove = new HashSet<>();
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == '(') {
-                stack.push(i);
-            } else if(c == ')') {
-                if(stack.isEmpty()) {
-                    indicesToRemove.add(i);
-                } else {
-                    stack.pop();
+            Set<Integer> indexes = new HashSet<>();
+            Stack<Integer> stack = new Stack<>();
+            for(int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+
+                if(c == '(') {
+                    stack.push(i);
+                } else if(c == ')') {
+                    if(!stack.isEmpty()) {
+                        stack.pop();
+                    } else {
+                        indexes.add(i);
+                    }
                 }
             }
-        }
 
-        while(!stack.isEmpty()) {
-            indicesToRemove.add(stack.pop());
-        }
-
-        StringBuilder result = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            if(indicesToRemove.contains(i)) {
-                continue;
+            while(!stack.isEmpty()) {
+                indexes.add(stack.pop());
             }
 
-            result.append(s.charAt(i));
-        }
+            StringBuilder builder = new StringBuilder();
+            for(int i = 0; i < s.length(); i++) {
+                if(indexes.contains(i)) {
+                    continue;
+                }
 
-        return result.toString();
+                builder.append(s.charAt(i));
+            }
+
+            return builder.toString();
+        }
+    }
+
+    class Solution_Correct_1 {
+        public String minRemoveToMakeValid(String s) {
+            if(s == null || s.length() == 0) {
+                return null;
+            }
+
+            Set<Integer> indicesToRemove = new HashSet<>();
+            Stack<Integer> stack = new Stack<>();
+            for(int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if(c == '(') {
+                    stack.push(i);
+                } else if(c == ')') {
+                    if(stack.isEmpty()) {
+                        indicesToRemove.add(i);
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            while(!stack.isEmpty()) {
+                indicesToRemove.add(stack.pop());
+            }
+
+            StringBuilder result = new StringBuilder();
+            for(int i = 0; i < s.length(); i++) {
+                if(indicesToRemove.contains(i)) {
+                    continue;
+                }
+
+                result.append(s.charAt(i));
+            }
+
+            return result.toString();
+        }
     }
 }
 
