1fc7c3b0709e3f7eef3dbd30ef8e5e07cbe52ac8
drjoewolfe <drjoewolfe@gmail.com>
2022-09-02 16:39:50 +0530

    637. Average of Levels in Binary Tree

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/AverageOfLevelsInBinaryTree.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/AverageOfLevelsInBinaryTree.java
@@ -6,38 +6,105 @@ import java.util.List;
 import java.util.Queue;
 
 public class AverageOfLevelsInBinaryTree {
-    public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> levelAverages = new ArrayList<>();
-        if(root == null) {
-            return levelAverages;
-        }
+    class Solution {
+        public List<Double> averageOfLevels(TreeNode root) {
+            List<Double> averages = new ArrayList<>();
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
 
-        while(!queue.isEmpty()) {
-            double levelSum = 0;
-            int levelSize = queue.size();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while(!queue.isEmpty()) {
+                int size = queue.size();
+                double sum = 0;
 
-            for(int i = 0; i < levelSize; i++) {
-                TreeNode node = queue.poll();
-                levelSum += node.val;
+                for(int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    sum += node.val;
 
-                if(node.left != null) {
-                    queue.offer(node.left);
+                    if(node.left != null) {
+                        queue.offer(node.left);
+                    }
+
+                    if(node.right != null) {
+                        queue.offer(node.right);
+                    }
                 }
 
-                if(node.right != null) {
-                    queue.offer(node.right);
-                }
+                double average = sum / size;
+                averages.add(average);
             }
 
-            double levelAvg = levelSum / levelSize;
-            levelAverages.add(levelAvg);
+            return averages;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public List<Double> averageOfLevels(TreeNode root) {
+            List<Double> averages = new ArrayList<>();
+
+            Map<Integer, Double> levelSums = new HashMap<>();
+            Map<Integer, Integer> levelCounts = new HashMap<>();
+            traverse(root, 0, levelSums, levelCounts);
+
+            for(int i = 0; i < levelSums.size(); i++) {
+                double levelAverage = levelSums.get(i) / levelCounts.get(i);
+                averages.add(levelAverage);
+            }
+
+
+            return averages;
         }
 
-        return levelAverages;
+        private void traverse(TreeNode root, int level, Map<Integer, Double> levelSums, Map<Integer, Integer> levelCounts) {
+            if(root == null) {
+                return;
+            }
+
+            levelSums.put(level, levelSums.getOrDefault(level, 0d) + root.val);
+            levelCounts.put(level, levelCounts.getOrDefault(level, 0) + 1);
+
+            traverse(root.left, level + 1, levelSums, levelCounts);
+            traverse(root.right, level + 1, levelSums, levelCounts);
+        }
     }
+
+    class Solution_Correct_BFS {
+        public List<Double> averageOfLevels(TreeNode root) {
+            List<Double> levelAverages = new ArrayList<>();
+            if(root == null) {
+                return levelAverages;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while(!queue.isEmpty()) {
+                double levelSum = 0;
+                int levelSize = queue.size();
+
+                for(int i = 0; i < levelSize; i++) {
+                    TreeNode node = queue.poll();
+                    levelSum += node.val;
+
+                    if(node.left != null) {
+                        queue.offer(node.left);
+                    }
+
+                    if(node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+
+                double levelAvg = levelSum / levelSize;
+                levelAverages.add(levelAvg);
+            }
+
+            return levelAverages;
+        }
+    }
+
+// [3,9,20,null,null,15,7]
+// [2147483647,2147483647,2147483647]
 
     public class TreeNode {
         int val;
