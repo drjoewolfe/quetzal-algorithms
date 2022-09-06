3da670ba7399fbed09ff22938dd4177e663a4779
drjoewolfe <drjoewolfe@gmail.com>
2022-09-06 19:38:20 +0530

    814. Binary Tree Pruning

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/BinaryTreePruning.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/BinaryTreePruning.java
@@ -12,6 +12,23 @@ public class BinaryTreePruning {
      */
     class Solution {
         public TreeNode pruneTree(TreeNode root) {
+            if(root == null) {
+                return null;
+            }
+
+            root.left = pruneTree(root.left);
+            root.right = pruneTree(root.right);
+
+            if(root.left == null && root.right == null && root.val != 1) {
+                return null;
+            }
+
+            return root;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public TreeNode pruneTree(TreeNode root) {
 
             if(root == null) {
                 return null;
