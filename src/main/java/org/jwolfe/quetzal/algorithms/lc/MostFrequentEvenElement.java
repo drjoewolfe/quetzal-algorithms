8ccff65372231141def49324b5e5687383b55d75
drjoewolfe <drjoewolfe@gmail.com>
2022-09-19 03:40:38 +0530

    2404. Most Frequent Even Element

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MostFrequentEvenElement.java
@@ -0,0 +1,72 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MostFrequentEvenElement {
+    class Solution {
+        public int mostFrequentEven(int[] nums) {
+            if(nums == null || nums.length == 0) {
+                return -1;
+            }
+
+            Map<Integer, Integer> map = new HashMap<>();
+            int maxFrequency = 0;
+            for(int a : nums) {
+                if(a % 2 == 0) {
+                    map.put(a, map.getOrDefault(a, 0) + 1);
+                    maxFrequency = Math.max(maxFrequency, map.get(a));
+                }
+            }
+
+            if(map.size() == 0) {
+                return -1;
+            }
+
+            int mostFrequentElement = Integer.MAX_VALUE;
+            for(var entry : map.entrySet()) {
+                if(entry.getValue() < maxFrequency) {
+                    continue;
+                }
+
+                mostFrequentElement = Math.min(mostFrequentElement, entry.getKey());
+            }
+
+            return mostFrequentElement;
+        }
+    }
+
+// [0,1,2,2,4,4,1]
+}
+
+//    2404. Most Frequent Even Element
+//    Easy
+//    Given an integer array nums, return the most frequent even element.
+//
+//    If there is a tie, return the smallest one. If there is no such element, return -1.
+//
+//
+//
+//    Example 1:
+//
+//    Input: nums = [0,1,2,2,4,4,1]
+//    Output: 2
+//    Explanation:
+//    The even elements are 0, 2, and 4. Of these, 2 and 4 appear the most.
+//    We return the smallest one, which is 2.
+//    Example 2:
+//
+//    Input: nums = [4,4,4,9,2,4]
+//    Output: 4
+//    Explanation: 4 is the even element appears the most.
+//    Example 3:
+//
+//    Input: nums = [29,47,21,41,13,37,25,7]
+//    Output: -1
+//    Explanation: There is no even element.
+//
+//
+//    Constraints:
+//
+//    1 <= nums.length <= 2000
+//    0 <= nums[i] <= 105
