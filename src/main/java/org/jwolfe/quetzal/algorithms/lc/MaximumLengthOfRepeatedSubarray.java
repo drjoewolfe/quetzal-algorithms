d70b6c5c501f76c3e4ef9c2d7934455f2374bf46
drjoewolfe <drjoewolfe@gmail.com>
2022-09-21 01:58:13 +0530

    718. Maximum Length of Repeated Subarray

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/MaximumLengthOfRepeatedSubarray.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MaximumLengthOfRepeatedSubarray.java
@@ -3,6 +3,31 @@ package org.jwolfe.quetzal.algorithms.lc;
 public class MaximumLengthOfRepeatedSubarray {
     class Solution {
         public int findLength(int[] A, int[] B) {
+            if(A == null || B == null || A.length == 0 || B.length == 0) {
+                return 0;
+            }
+
+            int m = A.length;
+            int n = B.length;
+
+            int[][] dp = new int[m + 1][n + 1];
+            int maxLength = 0;
+
+            for(int i = m - 1; i >= 0; i--) {
+                for(int j = n - 1; j >= 0; j--) {
+                    if(A[i] == B[j]) {
+                        dp[i][j] = dp[i + 1][j + 1] + 1;
+                        maxLength = Math.max(maxLength, dp[i][j]);
+                    }
+                }
+            }
+
+            return maxLength;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public int findLength(int[] A, int[] B) {
             if(A == null || A.length == 0 || B == null || B.length == 0) {
                 return 0;
             }
