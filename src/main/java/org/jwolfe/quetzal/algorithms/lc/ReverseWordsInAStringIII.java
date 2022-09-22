50dd0256dded6ec2bd4c698b59eda8e06145af33
drjoewolfe <drjoewolfe@gmail.com>
2022-09-23 03:55:27 +0530

    557. Reverse Words in a String III

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/ReverseWordsInAStringIII.java
@@ -0,0 +1,82 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class ReverseWordsInAStringIII {
+    class Solution {
+        public String reverseWords(String s) {
+            if(s == null || s.length() == 0) {
+                return s;
+            }
+
+            StringBuilder builder = new StringBuilder(s);
+            int left = 0;
+            int right = 0;
+            for(; right < s.length(); right++) {
+                char c = s.charAt(right);
+                if(c == ' ') {
+                    reverse(builder, left, right - 1);
+
+                    left = right + 1;
+                }
+            }
+
+            if(left < right) {
+                reverse(builder, left, right - 1);
+            }
+
+            return builder.toString();
+        }
+
+        private void reverse(StringBuilder builder, int left, int right) {
+            while(left < right) {
+                char temp = builder.charAt(left);
+                builder.setCharAt(left, builder.charAt(right));
+                builder.setCharAt(right, temp);
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    class Solution_Correct_1 {
+        public String reverseWords(String s) {
+            if(s == null || s.length() == 0) {
+                return s;
+            }
+
+            String[] words = s.split(" ");
+            StringBuilder builder = new StringBuilder();
+            for(String w : words) {
+                builder.append(new StringBuilder(w).reverse().toString());
+                builder.append(" ");
+            }
+
+            builder.deleteCharAt(builder.length() - 1);
+            return builder.toString();
+        }
+    }
+}
+
+//    557. Reverse Words in a String III
+//    Easy
+//    Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+//
+//
+//
+//    Example 1:
+//
+//    Input: s = "Let's take LeetCode contest"
+//    Output: "s'teL ekat edoCteeL tsetnoc"
+//    Example 2:
+//
+//    Input: s = "God Ding"
+//    Output: "doG gniD"
+//
+//
+//    Constraints:
+//
+//    1 <= s.length <= 5 * 104
+//    s contains printable ASCII characters.
+//    s does not contain any leading or trailing spaces.
+//    There is at least one word in s.
+//    All the words in s are separated by a single space.
