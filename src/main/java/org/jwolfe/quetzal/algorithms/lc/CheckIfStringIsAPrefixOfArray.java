0aee0d47fbd580a7c7a10a2080680fa1f36770d6
drjoewolfe <drjoewolfe@gmail.com>
2022-10-05 22:36:25 +0530

    1961. Check If String Is a Prefix of Array

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CheckIfStringIsAPrefixOfArray.java
@@ -0,0 +1,68 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class CheckIfStringIsAPrefixOfArray {
+    class Solution {
+        public boolean isPrefixString(String s, String[] words) {
+            if(words == null || words.length == 0) {
+                return false;
+            }
+
+            if(s == null || s.length() == 0) {
+                return true;
+            }
+
+            int n = s.length();
+            int index = 0;
+            for(String candidate : words) {
+                int cn = candidate.length();
+
+                if(index + cn > n){
+                    return false;
+                }
+
+                String str = s.substring(index, index + cn);
+                if(!str.equals(candidate)) {
+                    return false;
+                }
+
+                index += cn;
+                if(index == n) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
+//    1961. Check If String Is a Prefix of Array
+//    Easy
+//    Given a string s and an array of strings words, determine whether s is a prefix string of words.
+//
+//    A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
+//
+//    Return true if s is a prefix string of words, or false otherwise.
+//
+//
+//
+//    Example 1:
+//
+//    Input: s = "iloveleetcode", words = ["i","love","leetcode","apples"]
+//    Output: true
+//    Explanation:
+//    s can be made by concatenating "i", "love", and "leetcode" together.
+//    Example 2:
+//
+//    Input: s = "iloveleetcode", words = ["apples","i","love","leetcode"]
+//    Output: false
+//    Explanation:
+//    It is impossible to make s using a prefix of arr.
+//
+//
+//    Constraints:
+//
+//    1 <= words.length <= 100
+//    1 <= words[i].length <= 20
+//    1 <= s.length <= 1000
+//    words[i] and s consist of only lowercase English letters.
