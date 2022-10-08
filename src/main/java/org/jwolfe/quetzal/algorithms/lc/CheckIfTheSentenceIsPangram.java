562249a08150b339fb50d5a26b47cce576691860
drjoewolfe <drjoewolfe@gmail.com>
2022-10-09 02:32:15 +0530

    1832. Check if the Sentence Is Pangram

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,48 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class CheckIfTheSentenceIsPangram {
+    class Solution {
+        public boolean checkIfPangram(String sentence) {
+            if(sentence == null || sentence.length() < 26) {
+                return false;
+            }
+
+            int[] frequencies = new int[26];
+            for(int i = 0; i < sentence.length(); i++) {
+                frequencies[sentence.charAt(i) - 'a']++;
+            }
+
+            for(int i = 0; i < 26; i++) {
+                if(frequencies[i] == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
+//    1832. Check if the Sentence Is Pangram
+//    Easy
+//    A pangram is a sentence where every letter of the English alphabet appears at least once.
+//
+//    Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
+//
+//
+//
+//    Example 1:
+//
+//    Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+//    Output: true
+//    Explanation: sentence contains at least one of every letter of the English alphabet.
+//    Example 2:
+//
+//    Input: sentence = "leetcode"
+//    Output: false
+//
+//
+//    Constraints:
+//
+//    1 <= sentence.length <= 1000
+//    sentence consists of lowercase English letters.
