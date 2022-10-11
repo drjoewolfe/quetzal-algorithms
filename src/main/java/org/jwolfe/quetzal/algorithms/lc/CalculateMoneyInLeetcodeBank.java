94bf394ac4ebd3051c64c611b03a05abb4a737eb
drjoewolfe <drjoewolfe@gmail.com>
2022-10-11 15:59:14 +0530

    1716. Calculate Money in Leetcode Bank

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CalculateMoneyInLeetcodeBank.java
@@ -0,0 +1,79 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class CalculateMoneyInLeetcodeBank {
+    class Solution {
+        public int totalMoney(int n) {
+            if(n < 1) {
+                return 0;
+            }
+
+            int totalAmount = 0;
+
+            // In 1 week, 1+2+3+4+5+6+7 = 28
+            int wholeWeeks = n / 7;
+            for(int w = 1; w <= wholeWeeks; w++) {
+                totalAmount += (28 + (w - 1) * 7);
+            }
+
+            int remainingDays = n % 7;
+            for(int d = 1; d <= remainingDays; d++) {
+                totalAmount += (d + wholeWeeks);
+            }
+
+            return totalAmount;
+        }
+    }
+
+    class Solution_Approach_1 {
+        public int totalMoney(int n) {
+            if(n < 1) {
+                return 0;
+            }
+
+            int totalAmount = 0;
+            int w = 1;
+            while(n > 0) {
+                int amount = w;
+                for(int d = 1; d <= 7 && n > 0; d++) {
+                    totalAmount += amount;
+                    amount++;
+                    n--;
+                }
+
+                w++;
+            }
+
+            return totalAmount;
+        }
+    }
+}
+
+//    1716. Calculate Money in Leetcode Bank
+//    Easy
+//    Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+//
+//    He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+//    Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+//
+//
+//
+//    Example 1:
+//
+//    Input: n = 4
+//    Output: 10
+//    Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+//    Example 2:
+//
+//    Input: n = 10
+//    Output: 37
+//    Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+//    Example 3:
+//
+//    Input: n = 20
+//    Output: 96
+//    Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+//
+//
+//    Constraints:
+//
+//    1 <= n <= 1000
