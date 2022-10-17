6a6e732bb469f09b77645aad18797ee1087867a1
drjoewolfe <drjoewolfe@gmail.com>
2022-10-17 14:07:38 +0530

    1832. Check if the Sentence Is Pangram

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/CheckIfTheSentenceIsPangram.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CheckIfTheSentenceIsPangram.java
@@ -1,7 +1,50 @@
 package org.jwolfe.quetzal.algorithms.lc;
 
+import java.util.stream.Collectors;
+
 public class CheckIfTheSentenceIsPangram {
     class Solution {
+        public boolean checkIfPangram(String sentence) {
+            if(sentence == null || sentence.length() < 26) {
+                return false;
+            }
+
+            return sentence.chars().boxed().collect(Collectors.toSet()).size() == 26;
+        }
+    }
+
+    class Solution_Correct_3 {
+        public boolean checkIfPangram(String sentence) {
+            if(sentence == null || sentence.length() < 26) {
+                return false;
+            }
+
+            return sentence.chars().distinct().count() == 26;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public boolean checkIfPangram(String sentence) {
+            if(sentence == null || sentence.length() < 26) {
+                return false;
+            }
+
+            int[] frequencies = new int[26];
+            for(int i = 0; i < sentence.length(); i++) {
+                frequencies[sentence.charAt(i) - 'a']++;
+            }
+
+            for(int i = 0; i < 26; i++) {
+                if(frequencies[i] == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Solution_Correct_1 {
         public boolean checkIfPangram(String sentence) {
             if(sentence == null || sentence.length() < 26) {
                 return false;
