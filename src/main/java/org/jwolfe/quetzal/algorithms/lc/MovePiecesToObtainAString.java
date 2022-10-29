2c88db1ef020525f1d74b12e349f23367fc1ed86
drjoewolfe <drjoewolfe@gmail.com>
2022-10-30 02:18:15 +0530

    2337. Move Pieces to Obtain a String

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MovePiecesToObtainAString.java
@@ -0,0 +1,99 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class MovePiecesToObtainAString {
+    class Solution {
+        public boolean canChange(String start, String target) {
+            if(start == null || target == null || start.length() != target.length()) {
+                return false;
+            }
+
+            int n = start.length();
+            int si = 0;
+            int ti = 0;
+
+            while(si < n || ti < n) {
+                while(si < n && start.charAt(si) == '_') {
+                    si++;
+                }
+
+                while(ti < n && target.charAt(ti) == '_') {
+                    ti++;
+                }
+
+                if(si == n && ti == n) {
+                    return true;
+                }
+
+                if(si == n || ti == n) {
+                    return false;
+                }
+
+                char sc = start.charAt(si);
+                char tc = target.charAt(ti);
+
+                if(sc != tc) {
+                    return false;
+                }
+
+                if(sc == 'L') {
+                    if(si < ti) {
+                        return false;
+                    }
+                } else {
+                    if(si > ti) {
+                        return false;
+                    }
+                }
+
+                si++;
+                ti++;
+            }
+
+            return si == n && ti == n;
+        }
+    }
+
+// "_L__R__R_"
+// "L______RR"
+
+// "_L"
+// "LL"
+}
+
+//    2337. Move Pieces to Obtain a String
+//    Medium
+//    You are given two strings start and target, both of length n. Each string consists only of the characters 'L', 'R', and '_' where:
+//
+//    The characters 'L' and 'R' represent pieces, where a piece 'L' can move to the left only if there is a blank space directly to its left, and a piece 'R' can move to the right only if there is a blank space directly to its right.
+//    The character '_' represents a blank space that can be occupied by any of the 'L' or 'R' pieces.
+//    Return true if it is possible to obtain the string target by moving the pieces of the string start any number of times. Otherwise, return false.
+//
+//
+//
+//    Example 1:
+//
+//    Input: start = "_L__R__R_", target = "L______RR"
+//    Output: true
+//    Explanation: We can obtain the string target from start by doing the following moves:
+//    - Move the first piece one step to the left, start becomes equal to "L___R__R_".
+//    - Move the last piece one step to the right, start becomes equal to "L___R___R".
+//    - Move the second piece three steps to the right, start becomes equal to "L______RR".
+//    Since it is possible to get the string target from start, we return true.
+//    Example 2:
+//
+//    Input: start = "R_L_", target = "__LR"
+//    Output: false
+//    Explanation: The 'R' piece in the string start can move one step to the right to obtain "_RL_".
+//    After that, no pieces can move anymore, so it is impossible to obtain the string target from start.
+//    Example 3:
+//
+//    Input: start = "_R", target = "R_"
+//    Output: false
+//    Explanation: The piece in the string start can move only to the right, so it is impossible to obtain the string target from start.
+//
+//
+//    Constraints:
+//
+//    n == start.length == target.length
+//    1 <= n <= 105
+//    start and target consist of the characters 'L', 'R', and '_'.
