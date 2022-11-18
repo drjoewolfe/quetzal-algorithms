9d3e2bf2b2180a10f797ba5455c6a32768643bb9
drjoewolfe <drjoewolfe@gmail.com>
2022-11-18 20:22:57 +0530

    263. Ugly Number

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/UglyNumber.java
@@ -0,0 +1,76 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class UglyNumber {
+    class Solution {
+        public boolean isUgly(int num) {
+            if(num <= 0) {
+                return false;
+            }
+
+            while(num % 5 == 0) {
+                num /= 5;
+            }
+
+            while(num % 3 == 0) {
+                num /= 3;
+            }
+
+            while(num % 2 == 0) {
+                num /= 2;
+            }
+
+            return num == 1;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public boolean isUgly(int num) {
+            if(num < 1) {
+                return false;
+            }
+
+            while(num % 2 == 0) {
+                num /= 2;
+            }
+
+            while(num % 3 == 0) {
+                num /= 3;
+            }
+
+            while(num % 5 == 0) {
+                num /= 5;
+            }
+
+            return num == 1;
+        }
+    }
+}
+
+//    263. Ugly Number
+//    Easy
+//    An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+//
+//    Given an integer n, return true if n is an ugly number.
+//
+//
+//
+//    Example 1:
+//
+//    Input: n = 6
+//    Output: true
+//    Explanation: 6 = 2 × 3
+//    Example 2:
+//
+//    Input: n = 1
+//    Output: true
+//    Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+//    Example 3:
+//
+//    Input: n = 14
+//    Output: false
+//    Explanation: 14 is not ugly since it includes the prime factor 7.
+//
+//
+//    Constraints:
+//
+//    -231 <= n <= 231 - 1
