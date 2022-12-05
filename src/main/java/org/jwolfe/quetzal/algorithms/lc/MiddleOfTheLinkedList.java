921eb6af2cbfda235609b93dfc3ade81b1ba250f
drjoewolfe <drjoewolfe@gmail.com>
2022-12-05 17:51:52 +0530

    876. Middle of the Linked List

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/MiddleOfTheLinkedList.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MiddleOfTheLinkedList.java
@@ -14,7 +14,21 @@ public class MiddleOfTheLinkedList {
             ListNode fast = head;
             ListNode slow = head;
 
-            while (fast != null && fast.next != null) {
+            while(fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            return slow;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public ListNode middleNode(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while(fast != null && fast.next != null) {
                 fast = fast.next.next;
                 slow = slow.next;
             }
