6c3062752357928ed1e094a3a330126ec3e22bb7
drjoewolfe <drjoewolfe@gmail.com>
2023-01-01 23:47:54 +0530

    2520. Count the Digits That Divide a Number

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CountTheDigitsThatDivideANumber.java
@@ -0,0 +1,51 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class CountTheDigitsThatDivideANumber {
+    class Solution {
+        public int countDigits(int num) {
+            int count = 0;
+
+            int copy = num;
+            while(copy > 0) {
+                int digit = copy % 10;
+                if(num % digit == 0) {
+                    count++;
+                }
+
+                copy /= 10;
+            }
+
+            return count;
+        }
+    }
+}
+
+//    2520. Count the Digits That Divide a Number
+//    Easy
+//    Given an integer num, return the number of digits in num that divide num.
+//
+//    An integer val divides nums if nums % val == 0.
+//
+//
+//
+//    Example 1:
+//
+//    Input: num = 7
+//    Output: 1
+//    Explanation: 7 divides itself, hence the answer is 1.
+//    Example 2:
+//
+//    Input: num = 121
+//    Output: 2
+//    Explanation: 121 is divisible by 1, but not 2. Since 1 occurs twice as a digit, we return 2.
+//    Example 3:
+//
+//    Input: num = 1248
+//    Output: 4
+//    Explanation: 1248 is divisible by all of its digits, hence the answer is 4.
+//
+//
+//    Constraints:
+//
+//    1 <= num <= 109
+//    num does not contain 0 as one of its digits.
