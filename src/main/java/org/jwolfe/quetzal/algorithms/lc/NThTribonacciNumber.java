667926dbf7fc37b5477542a69a0b8ff6f1e81362
drjoewolfe <drjoewolfe@gmail.com>
2023-01-30 13:26:05 +0530

    1137. N-th Tribonacci Number

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/NThTribonacciNumber.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/NThTribonacciNumber.java
@@ -5,6 +5,31 @@ import java.util.Arrays;
 public class NThTribonacciNumber {
     class Solution {
         public int tribonacci(int n) {
+            if(n < 1) {
+                return 0;
+            }
+
+            if(n < 3) {
+                return 1;
+            }
+
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            for(int i = 3; i <= n; i++) {
+                int d = c + b + a;
+
+                a = b;
+                b = c;
+                c = d;
+            }
+
+            return c;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public int tribonacci(int n) {
             if(n <= 0) {
                 return 0;
             }
