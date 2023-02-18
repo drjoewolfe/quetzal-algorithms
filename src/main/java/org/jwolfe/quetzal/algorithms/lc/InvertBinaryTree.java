7e5f953c86a4fdb5556cec13b177bbfe4e8dfa74
drjoewolfe <drjoewolfe@gmail.com>
2023-02-18 17:19:31 +0530

    226. Invert Binary Tree

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/InvertBinaryTree.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/InvertBinaryTree.java
@@ -14,6 +14,23 @@ public class InvertBinaryTree {
     class Solution {
         public TreeNode invertTree(TreeNode root) {
             if(root == null) {
+                return root;
+            }
+
+            invertTree(root.left);
+            invertTree(root.right);
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+
+            return root;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public TreeNode invertTree(TreeNode root) {
+            if(root == null) {
                 return null;
             }
 
