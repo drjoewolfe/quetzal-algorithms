563768cef059e5544222df8baf0fbc74a349591d
drjoewolfe <drjoewolfe@gmail.com>
2023-02-21 20:04:45 +0530

    540. Single Element in a Sorted Array

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/SingleElementInASortedArray.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/SingleElementInASortedArray.java
@@ -3,6 +3,43 @@ package org.jwolfe.quetzal.algorithms.lc;
 public class SingleElementInASortedArray {
     class Solution {
         public int singleNonDuplicate(int[] nums) {
+            if(nums == null || nums.length % 2 == 0) {
+                return -1;
+            }
+
+            int n = nums.length;
+
+            int left = 0;
+            int right = n - 1;
+
+            while(left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if(mid < n - 1
+                        && nums[mid] == nums[mid + 1]) {
+                    if((n - mid) % 2 == 0) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else if(mid > 0
+                        && nums[mid - 1] == nums[mid]) {
+                    if((n - mid + 1) % 2 == 0) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else {
+                    return nums[mid];
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public int singleNonDuplicate(int[] nums) {
             if(nums == null || nums.length == 0 || nums.length % 2 == 0) {
                 return -1;
             }
