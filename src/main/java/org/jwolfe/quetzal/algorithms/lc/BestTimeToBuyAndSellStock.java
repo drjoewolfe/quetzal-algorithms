665e57aeac31f2524c6ca86ec7904903d144baf6
drjoewolfe <drjoewolfe@gmail.com>
2023-02-25 21:05:24 +0530

    121. Best Time to Buy and Sell Stock

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/BestTimeToBuyAndSellStock.java
@@ -7,6 +7,26 @@ public class BestTimeToBuyAndSellStock {
                 return 0;
             }
 
+            int maxProfit = 0;
+            int buy = prices[0];
+
+            for(int i = 1; i < prices.length; i++) {
+                int sell = prices[i];
+                maxProfit = Math.max(maxProfit, sell - buy);
+
+                buy = Math.min(buy, sell);
+            }
+
+            return maxProfit;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public int maxProfit(int[] prices) {
+            if(prices == null || prices.length < 2) {
+                return 0;
+            }
+
             int min = prices[0];
             int maxProfit = 0;
             for(int i = 1; i < prices.length; i++) {
