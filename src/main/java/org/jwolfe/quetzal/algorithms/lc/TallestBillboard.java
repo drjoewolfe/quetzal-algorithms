a169b535b3fbf2e5782cc6a2773205d24e0c3eae
drjoewolfe <drjoewolfe@gmail.com>
2023-06-25 05:03:36 +0530

    956. Tallest Billboard

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/TallestBillboard.java
@@ -0,0 +1,82 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TallestBillboard {
+    class Solution {
+        public int tallestBillboard(int[] rods) {
+            if(rods == null || rods.length < 2) {
+                return 0;
+            }
+
+            int n = rods.length;
+
+            // dp stores (diff, taller)
+            Map<Integer, Integer> dp = new HashMap<>();
+            dp.put(0, 0);
+
+            for(int rod : rods) {
+                Map<Integer, Integer> newDp = new HashMap<>(dp);
+
+                for(var entry : dp.entrySet()) {
+                    int diff = entry.getKey();
+                    int taller = entry.getValue();
+                    int shorter = taller - diff;
+
+                    // add to shorter
+                    int newDiff = Math.abs(shorter + rod - taller);
+                    int newTaller = Math.max(newDp.getOrDefault(newDiff, 0), Math.max(shorter + rod, taller));
+                    newDp.put(newDiff, newTaller);
+
+
+                    // add to taller
+                    newDiff = diff + rod;
+                    newTaller = Math.max(newDp.getOrDefault(newDiff, 0), taller + rod);
+                    newDp.put(newDiff, newTaller);
+
+                }
+
+                dp = newDp;
+            }
+
+
+            return dp.get(0);
+        }
+    }
+
+// [1,2,3,6]
+}
+
+//    956. Tallest Billboard
+//    Hard
+//    You are installing a billboard and want it to have the largest height. The billboard will have two steel supports, one on each side. Each steel support must be an equal height.
+//
+//    You are given a collection of rods that can be welded together. For example, if you have rods of lengths 1, 2, and 3, you can weld them together to make a support of length 6.
+//
+//    Return the largest possible height of your billboard installation. If you cannot support the billboard, return 0.
+//
+//
+//
+//    Example 1:
+//
+//    Input: rods = [1,2,3,6]
+//    Output: 6
+//    Explanation: We have two disjoint subsets {1,2,3} and {6}, which have the same sum = 6.
+//    Example 2:
+//
+//    Input: rods = [1,2,3,4,5,6]
+//    Output: 10
+//    Explanation: We have two disjoint subsets {2,3,5} and {4,6}, which have the same sum = 10.
+//    Example 3:
+//
+//    Input: rods = [1,2]
+//    Output: 0
+//    Explanation: The billboard cannot be supported, so we return 0.
+//
+//
+//    Constraints:
+//
+//    1 <= rods.length <= 20
+//    1 <= rods[i] <= 1000
+//    sum(rods[i]) <= 5000
