41f41a9ae9316e7c6d1ee3640827379a90ceab92
drjoewolfe <drjoewolfe@gmail.com>
2023-09-04 16:09:04 +0530

    141. Linked List Cycle

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/LinkedListCycle.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/LinkedListCycle.java
@@ -14,18 +14,36 @@ public class LinkedListCycle {
      */
     public class Solution {
         public boolean hasCycle(ListNode head) {
-            if (head == null) {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while(fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if(fast == slow) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public boolean hasCycle(ListNode head) {
+            if(head == null) {
                 return false;
             }
 
             ListNode fast = head;
             ListNode slow = head;
 
-            while (fast.next != null && fast.next.next != null) {
+            while(fast.next != null && fast.next.next != null) {
                 fast = fast.next.next;
                 slow = slow.next;
 
-                if (fast == slow) {
+                if(fast == slow) {
                     return true;
                 }
             }
