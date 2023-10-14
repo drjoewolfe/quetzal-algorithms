22896b83e08b0e98939b11c94f704e81e0e643d3
drjoewolfe <drjoewolfe@gmail.com>
2023-10-15 01:08:34 +0530

    2652. Sum Multiples

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/SumMultiples.java
@@ -0,0 +1,45 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class SumMultiples {
+    class Solution {
+        public int sumOfMultiples(int n) {
+            int sum = 0;
+            for(int a = 1; a <= n; a++) {
+                if(a % 3 == 0 || a % 5 == 0 || a % 7 == 0) {
+                    sum += a;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
+
+//    2652. Sum Multiples
+//    Easy
+//    Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
+//
+//    Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+//
+//
+//
+//    Example 1:
+//
+//    Input: n = 7
+//    Output: 21
+//    Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
+//    Example 2:
+//
+//    Input: n = 10
+//    Output: 40
+//    Explanation: Numbers in the range [1, 10] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9, 10. The sum of these numbers is 40.
+//    Example 3:
+//
+//    Input: n = 9
+//    Output: 30
+//    Explanation: Numbers in the range [1, 9] that are divisible by 3, 5, or 7 are 3, 5, 6, 7, 9. The sum of these numbers is 30.
+//
+//
+//    Constraints:
+//
+//    1 <= n <= 103
