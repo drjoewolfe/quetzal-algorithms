66453761178b0a79ec213bf4ce0bd5de731e4f19
drjoewolfe <drjoewolfe@gmail.com>
2024-01-03 02:09:05 +0530

    503. Next Greater Element II

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/NextGreaterElementII.java
@@ -0,0 +1,58 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.Stack;
+
+public class NextGreaterElementII {
+    class Solution {
+        public int[] nextGreaterElements(int[] nums) {
+            if(nums == null|| nums.length == 0) {
+                return new int[0];
+            }
+
+            int n = nums.length;
+            int[] results = new int[n];
+            Stack<Integer> stack = new Stack<>();
+
+            for(int i = 2 * n - 1; i >= 0; i--) {
+                int index = i % n;
+                int val = nums[index];
+
+                while(!stack.isEmpty()
+                        && stack.peek() <= val) {
+                    stack.pop();
+                }
+
+                results[index] = stack.isEmpty() ? -1 : stack.peek();
+                stack.push(val);
+            }
+
+            return results;
+        }
+    }
+}
+
+//    503. Next Greater Element II
+//    Medium
+//    Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+//
+//    The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+//
+//
+//
+//    Example 1:
+//
+//    Input: nums = [1,2,1]
+//    Output: [2,-1,2]
+//    Explanation: The first 1's next greater number is 2;
+//    The number 2 can't find next greater number.
+//    The second 1's next greater number needs to search circularly, which is also 2.
+//    Example 2:
+//
+//    Input: nums = [1,2,3,4,3]
+//    Output: [2,3,4,-1,4]
+//
+//
+//    Constraints:
+//
+//    1 <= nums.length <= 104
+//    -109 <= nums[i] <= 109
