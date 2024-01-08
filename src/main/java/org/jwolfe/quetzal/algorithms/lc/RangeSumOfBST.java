ff9e45b170cc5121eb5f9f10f320b046ef9e4423
drjoewolfe <drjoewolfe@gmail.com>
2024-01-08 19:09:09 +0530

    Range Sum of BST

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/RangeSumOfBST.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/RangeSumOfBST.java
@@ -23,6 +23,30 @@ public class RangeSumOfBST {
             }
 
             int sum = 0;
+
+            if(root.val >= low && root.val <= high) {
+                sum += root.val;
+            }
+
+            if(root.val > low) {
+                sum += rangeSumBST(root.left, low, high);
+            }
+
+            if(root.val < high) {
+                sum += rangeSumBST(root.right, low, high);
+            }
+
+            return sum;
+        }
+    }
+
+    class Solution_Correct_3 {
+        public int rangeSumBST(TreeNode root, int low, int high) {
+            if(root == null) {
+                return 0;
+            }
+
+            int sum = 0;
             if(root.val >= low && root.val <= high) {
                 sum += root.val;
             }
