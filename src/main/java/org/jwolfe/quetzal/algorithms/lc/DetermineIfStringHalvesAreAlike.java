a117c0b1367d06e82d3697130b1214f3b7c55294
drjoewolfe <drjoewolfe@gmail.com>
2024-01-13 01:21:53 +0530

    1704. Determine if String Halves Are Alike

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/DetermineIfStringHalvesAreAlike.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/DetermineIfStringHalvesAreAlike.java
@@ -11,6 +11,44 @@ public class DetermineIfStringHalvesAreAlike {
                 return false;
             }
 
+            int n = s.length();
+            int count = 0;
+            for(int i = 0; i < n / 2; i++) {
+                if(isVowel(s.charAt(i))) {
+                    count++;
+                }
+            }
+
+            for(int i = n / 2; i < n; i++) {
+                if(isVowel(s.charAt(i))) {
+                    count--;
+                }
+            }
+
+            return count == 0;
+        }
+
+        private boolean isVowel(char c) {
+            return (c == 'a')
+                    || (c == 'e')
+                    || (c == 'i')
+                    || (c == 'o')
+                    || (c == 'u')
+                    || (c == 'A')
+                    || (c == 'E')
+                    || (c == 'I')
+                    || (c == 'O')
+                    || (c == 'U') ;
+
+        }
+    }
+
+    class Solution_Correct_2 {
+        public boolean halvesAreAlike(String s) {
+            if(s == null || s.length() == 0 || s.length() % 2 != 0) {
+                return false;
+            }
+
             Set<Character> set = new HashSet<>(Arrays.asList(new Character[] {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}));
 
             int n = s.length();
