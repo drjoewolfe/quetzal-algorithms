aab16a7d1740f8793054d0c73a04cc9d6f2fe2d7
drjoewolfe <drjoewolfe@gmail.com>
2024-01-21 17:28:56 +0530

    198. House Robber

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/HouseRobber.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/HouseRobber.java
@@ -7,6 +7,29 @@ public class HouseRobber {
                 return 0;
             }
 
+            int n = nums.length;
+            if(n == 1) {
+                return nums[0];
+            }
+
+            int[] dp = new int[n];
+            dp[0] = nums[0];
+            dp[1] = Math.max(nums[1], nums[0]);
+
+            for(int i = 2; i < n; i++) {
+                dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            }
+
+            return dp[n - 1];
+        }
+    }
+
+    class Solution_Correct_2 {
+        public int rob(int[] nums) {
+            if(nums == null || nums.length == 0) {
+                return 0;
+            }
+
             if(nums.length == 1) {
                 return nums[0];
             }
