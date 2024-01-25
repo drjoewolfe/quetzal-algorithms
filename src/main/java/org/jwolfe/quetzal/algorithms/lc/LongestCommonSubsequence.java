08103de3da81a072365f73255022b368b3431bbc
drjoewolfe <drjoewolfe@gmail.com>
2024-01-25 08:02:19 +0530

    1143. Longest Common Subsequence

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/LongestCommonSubsequence.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/LongestCommonSubsequence.java
@@ -30,6 +30,33 @@ public class LongestCommonSubsequence {
         }
     }
 
+    class Solution_Correct_1 {
+        public int longestCommonSubsequence(String text1, String text2) {
+            if(text1 == null || text2 == null || text1.length() == 0 || text2.length() == 0) {
+                return 0;
+            }
+
+            int m = text1.length();
+            int n = text2.length();
+
+            int[][] dp = new int[m + 1][n + 1];
+            for(int i = 1; i <= m; i++) {
+                for(int j = 1; j <= n; j++) {
+                    if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.max(
+                                dp[i - 1][j],
+                                dp[i][j - 1]
+                        );
+                    }
+                }
+            }
+
+            return dp[m][n];
+        }
+    }
+
     class Solution_Memoized {
         public int longestCommonSubsequence(String text1, String text2) {
             if(text1 == null || text2 == null || text1.length() == 0 || text2.length() == 0) {
