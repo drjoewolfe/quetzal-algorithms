0dd8629cc59636e0203319ce3117bc8a7de401ef
drjoewolfe <drjoewolfe@gmail.com>
2024-02-11 10:47:49 +0530

    1463. Cherry Pickup II

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/CherryPickupII.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CherryPickupII.java
@@ -13,6 +13,81 @@ public class CherryPickupII {
             int m = grid.length;
             int n = grid[0].length;
 
+            int[][][] dp = new int[m][n][n];
+
+            // Last Row
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < n; k++) {
+                    if(j == k) {
+                        dp[m - 1][j][k] = grid[m - 1][j];
+                    } else {
+                        dp[m - 1][j][k] = grid[m - 1][j] + grid[m - 1][k];
+                    }
+                }
+            }
+
+            int[] directions = new int[] {-1, 0, 1};
+
+            // Remaining Rows
+            for(int i = m - 2; i >= 0; i--) {
+
+                for(int j = 0; j < n; j++) {
+
+                    for(int dj : directions) {
+                        int nj = j + dj;
+                        if(nj < 0 || nj == n) {
+                            continue;
+                        }
+
+                        for(int k = 0; k < n; k++) {
+                            for(int dk : directions) {
+                                int nk = k + dk;
+                                if(nk < 0 || nk == n) {
+                                    continue;
+                                }
+
+                                if(j == k) {
+                                    dp[i][j][k] = Math.max(dp[i][j][k], dp[i + 1][nj][nk] + grid[i][j]);
+                                } else {
+                                    dp[i][j][k] = Math.max(dp[i][j][k], dp[i + 1][nj][nk] + grid[i][j] + grid[i][k]);
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            return dp[0][0][n - 1];
+        }
+
+        private void print(int[][][] arr) {
+            for(int[][] a : arr) {
+                System.out.print("[");
+                for(int[] b : a) {
+                    System.out.print("[");
+                    for(int c : b) {
+                        System.out.print(c + " ");
+                    }
+                    System.out.print("] ");
+                }
+                System.out.println("] ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+    class Solution_Memoized_3 {
+        public int cherryPickup(int[][] grid) {
+            if(grid == null || grid.length == 0 || grid[0].length == 0) {
+                return 0;
+            }
+
+            int m = grid.length;
+            int n = grid[0].length;
+
             for(var row : grid) {
                 if(row.length != n) {
                     return 0;
@@ -208,6 +283,8 @@ public class CherryPickupII {
             }
         }
     }
+
+
 
 // [[3,1,1],[2,5,1],[1,5,5],[2,1,1]]
 // [[16,1,9,16,5,16,3,16,6,3,1,7],[10,0,0,12,9,12,19,3,9,3,18,9],[18,6,6,13,2,1,9,8,12,2,10,6],[16,7,6,10,5,17,16,0,12,7,5,15],[20,11,17,15,2,8,12,2,17,13,12,0],[11,1,10,11,13,9,16,7,1,12,13,8],[12,19,19,3,13,0,7,1,1,3,1,16],[4,9,1,4,16,19,11,11,3,9,2,7],[10,13,1,4,3,7,14,3,20,7,6,11],[8,17,14,13,2,5,9,11,11,18,19,15],[16,4,3,13,18,17,14,16,15,12,20,13],[20,0,19,16,0,3,16,16,1,15,2,20],[20,18,8,11,0,13,8,7,13,6,18,19],[9,2,9,18,10,16,0,5,9,11,4,14],[11,4,18,8,18,18,10,12,11,0,10,13],[0,7,9,2,1,17,4,1,6,9,7,9]]
