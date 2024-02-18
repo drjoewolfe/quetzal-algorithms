d7ecb880646fab2dbb6d2cf1ae5fcbf37d943690
drjoewolfe <drjoewolfe@gmail.com>
2024-02-18 17:33:07 +0530

    3042. Count Prefix and Suffix Pairs I

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CountPrefixAndSuffixPairsI.java
@@ -0,0 +1,84 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class CountPrefixAndSuffixPairsI {
+    class Solution {
+        public int countPrefixSuffixPairs(String[] words) {
+            if(words == null || words.length == 0) {
+                return 0;
+            }
+
+            int count = 0;
+            int n = words.length;
+            for(int i = 0; i < n; i++) {
+                String str1 = words[i];
+                for(int j = i + 1; j < n; j++) {
+                    String str2 = words[j];
+                    if(str1.length() > str2.length()) {
+                        continue;
+                    }
+
+                    if(isPrefixAndSuffix(str1, str2)) {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private boolean isPrefixAndSuffix(String str1, String str2) {
+            if(str2.startsWith(str1) && str2.endsWith(str1)) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+// ["a","aba","ababa","aa"]
+}
+
+//    3042. Count Prefix and Suffix Pairs I
+//    Easy
+//    You are given a 0-indexed string array words.
+//
+//    Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
+//
+//    isPrefixAndSuffix(str1, str2) returns true if str1 is both a prefix and a suffix of str2, and false otherwise.
+//    For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix of "ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
+//
+//    Return an integer denoting the number of index pairs (i, j) such that i < j, and isPrefixAndSuffix(words[i], words[j]) is true.
+//
+//
+//
+//    Example 1:
+//
+//    Input: words = ["a","aba","ababa","aa"]
+//    Output: 4
+//    Explanation: In this example, the counted index pairs are:
+//    i = 0 and j = 1 because isPrefixAndSuffix("a", "aba") is true.
+//    i = 0 and j = 2 because isPrefixAndSuffix("a", "ababa") is true.
+//    i = 0 and j = 3 because isPrefixAndSuffix("a", "aa") is true.
+//    i = 1 and j = 2 because isPrefixAndSuffix("aba", "ababa") is true.
+//    Therefore, the answer is 4.
+//    Example 2:
+//
+//    Input: words = ["pa","papa","ma","mama"]
+//    Output: 2
+//    Explanation: In this example, the counted index pairs are:
+//    i = 0 and j = 1 because isPrefixAndSuffix("pa", "papa") is true.
+//    i = 2 and j = 3 because isPrefixAndSuffix("ma", "mama") is true.
+//    Therefore, the answer is 2.
+//    Example 3:
+//
+//    Input: words = ["abab","ab"]
+//    Output: 0
+//    Explanation: In this example, the only valid index pair is i = 0 and j = 1, and isPrefixAndSuffix("abab", "ab") is false.
+//    Therefore, the answer is 0.
+//
+//
+//    Constraints:
+//
+//    1 <= words.length <= 50
+//    1 <= words[i].length <= 10
+//    words[i] consists only of lowercase English letters.
