f1d19ed1e3d900b2c0c61030f527e59862cfcd9c
Appu Thampi <drjoewolfe@gmail.com>
2024-03-06 13:51:01 +0530

    141. Linked List Cycle

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/LinkedListCycle.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/LinkedListCycle.java
@@ -30,6 +30,24 @@ public class LinkedListCycle {
         }
     }
 
+    class Solution_Correct_3 {
+        public boolean hasCycle(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while(fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+
+                if(fast == slow) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
     class Solution_Correct_2 {
         public boolean hasCycle(ListNode head) {
             if(head == null) {
