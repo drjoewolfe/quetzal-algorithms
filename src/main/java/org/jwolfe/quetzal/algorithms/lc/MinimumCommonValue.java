771f64daca914210e9656cfc5733ccc7eeed5eb3
Appu Thampi <drjoewolfe@gmail.com>
2024-03-09 23:54:57 +0530

    2540. Minimum Common Value

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumCommonValue.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumCommonValue.java
@@ -11,6 +11,35 @@ public class MinimumCommonValue {
             }
 
             int i = 0;
+            int n1 = nums1.length;
+
+            int j = 0;
+            int n2 = nums2.length;
+
+            while(i < n1 && j < n2) {
+                if(nums1[i] == nums2[j]) {
+                    return nums1[i];
+                }
+
+                if(nums1[i] > nums2[j]) {
+                    j++;
+                } else {
+                    i++;
+                }
+            }
+
+
+            return -1;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public int getCommon(int[] nums1, int[] nums2) {
+            if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
+                return -1;
+            }
+
+            int i = 0;
             int j = 0;
 
             while(i < nums1.length && j < nums2.length) {
