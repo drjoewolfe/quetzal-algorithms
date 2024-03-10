14250fb1e977ff7759bdef0fba2cd3df2907c92b
Appu Thampi <drjoewolfe@gmail.com>
2024-03-11 00:24:36 +0530

    2644. Find the Maximum Divisibility Score

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/FindTheMaximumDivisibilityScore.java
@@ -0,0 +1,85 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class FindTheMaximumDivisibilityScore {
+    class Solution {
+        public int maxDivScore(int[] nums, int[] divisors) {
+            if(nums == null || divisors == null || nums.length == 0 || divisors.length == 0) {
+                return -1;
+            }
+
+            int maxScore = 0;
+            int maxIndex = 0;
+            for(int i = 0; i < divisors.length; i++) {
+                int divisor = divisors[i];
+
+                int score = 0;
+                for(int j = 0; j < nums.length; j++) {
+                    if(nums[j] % divisor == 0) {
+                        score++;
+                    }
+                }
+
+                if(score > maxScore) {
+                    maxScore = score;
+                    maxIndex = i;
+                } else if(score == maxScore) {
+                    maxIndex = divisors[i] < divisors[maxIndex] ? i : maxIndex;
+                }
+            }
+
+            return divisors[maxIndex];
+        }
+    }
+
+// [4,7,9,3,9]
+// [5,2,3]
+
+// [12]
+// [10,16]
+
+// [73,13,20,6]
+// [56,75,83,26,24,53,56,61]
+}
+
+//    2644. Find the Maximum Divisibility Score
+//    Easy
+//    You are given two 0-indexed integer arrays nums and divisors.
+//
+//    The divisibility score of divisors[i] is the number of indices j such that nums[j] is divisible by divisors[i].
+//
+//    Return the integer divisors[i] with the maximum divisibility score. If there is more than one integer with the maximum score, return the minimum of them.
+//
+//
+//
+//    Example 1:
+//
+//    Input: nums = [4,7,9,3,9], divisors = [5,2,3]
+//    Output: 3
+//    Explanation: The divisibility score for every element in divisors is:
+//    The divisibility score of divisors[0] is 0 since no number in nums is divisible by 5.
+//    The divisibility score of divisors[1] is 1 since nums[0] is divisible by 2.
+//    The divisibility score of divisors[2] is 3 since nums[2], nums[3], and nums[4] are divisible by 3.
+//    Since divisors[2] has the maximum divisibility score, we return it.
+//    Example 2:
+//
+//    Input: nums = [20,14,21,10], divisors = [5,7,5]
+//    Output: 5
+//    Explanation: The divisibility score for every element in divisors is:
+//    The divisibility score of divisors[0] is 2 since nums[0] and nums[3] are divisible by 5.
+//    The divisibility score of divisors[1] is 2 since nums[1] and nums[2] are divisible by 7.
+//    The divisibility score of divisors[2] is 2 since nums[0] and nums[3] are divisible by 5.
+//    Since divisors[0], divisors[1], and divisors[2] all have the maximum divisibility score, we return the minimum of them (i.e., divisors[2]).
+//    Example 3:
+//
+//    Input: nums = [12], divisors = [10,16]
+//    Output: 10
+//    Explanation: The divisibility score for every element in divisors is:
+//    The divisibility score of divisors[0] is 0 since no number in nums is divisible by 10.
+//    The divisibility score of divisors[1] is 0 since no number in nums is divisible by 16.
+//    Since divisors[0] and divisors[1] both have the maximum divisibility score, we return the minimum of them (i.e., divisors[0]).
+//
+//
+//    Constraints:
+//
+//    1 <= nums.length, divisors.length <= 1000
+//    1 <= nums[i], divisors[i] <= 109
