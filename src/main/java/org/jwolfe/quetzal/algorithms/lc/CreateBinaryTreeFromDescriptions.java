48fd3cd9d75b7ac632af03cc44d852441fa1fb57
Appu Thampi <drjoewolfe@gmail.com>
2024-07-16 02:06:43 +0530

    2196. Create Binary Tree From Descriptions

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CreateBinaryTreeFromDescriptions.java
@@ -0,0 +1,122 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CreateBinaryTreeFromDescriptions {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public TreeNode createBinaryTree(int[][] descriptions) {
+            if (descriptions == null || descriptions.length == 0) {
+                return null;
+            }
+
+            Map<Integer, TreeNode> nodeMap = new HashMap<>();
+            Set<Integer> allChildren = new HashSet<>();
+
+            for (int[] description : descriptions) {
+                int parent = description[0];
+                int child = description[1];
+                int isLeft = description[2];
+
+                if (!nodeMap.containsKey(parent)) {
+                    nodeMap.put(parent, new TreeNode(parent));
+                }
+
+                if (!nodeMap.containsKey(child)) {
+                    nodeMap.put(child, new TreeNode(child));
+                }
+
+                allChildren.add(child);
+
+                var parentNode = nodeMap.get(parent);
+                var childNode = nodeMap.get(child);
+
+                if (isLeft == 1) {
+                    parentNode.left = childNode;
+                } else {
+                    parentNode.right = childNode;
+                }
+            }
+
+            for (var candidate : nodeMap.keySet()) {
+                if (!allChildren.contains(candidate)) {
+                    return nodeMap.get(candidate);
+                }
+            }
+
+            return null;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+//    2196. Create Binary Tree From Descriptions
+//    Medium
+//    You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti] indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+//
+//    If isLefti == 1, then childi is the left child of parenti.
+//    If isLefti == 0, then childi is the right child of parenti.
+//    Construct the binary tree described by descriptions and return its root.
+//
+//    The test cases will be generated such that the binary tree is valid.
+//
+//
+//
+//    Example 1:
+//
+//
+//    Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+//    Output: [50,20,80,15,17,19]
+//    Explanation: The root node is the node with value 50 since it has no parent.
+//    The resulting binary tree is shown in the diagram.
+//    Example 2:
+//
+//
+//    Input: descriptions = [[1,2,1],[2,3,0],[3,4,1]]
+//    Output: [1,2,null,null,3,4]
+//    Explanation: The root node is the node with value 1 since it has no parent.
+//    The resulting binary tree is shown in the diagram.
+//
+//
+//    Constraints:
+//
+//    1 <= descriptions.length <= 104
+//    descriptions[i].length == 3
+//    1 <= parenti, childi <= 105
+//    0 <= isLefti <= 1
+//    The binary tree described by descriptions is valid.
