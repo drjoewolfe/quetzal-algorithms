208a60082ea8c75557d455e587320d4a36b32a9e
Appu Thampi <drjoewolfe@gmail.com>
2024-11-07 03:58:21 +0530

    3011. Find if Array Can Be Sorted

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/FindIfArrayCanBeSorted.java
@@ -0,0 +1,89 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class FindIfArrayCanBeSorted {
+    class Solution {
+        public boolean canSortArray(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return true;
+            }
+
+            int n = nums.length;
+
+            int currentBitCount = Integer.bitCount(nums[0]);
+            int segmentMax = nums[0];
+            int segmentMin = nums[0];
+
+            int prevSegmentMax = Integer.MIN_VALUE;
+
+            for (int i = 1; i < n; i++) {
+                int val = nums[i];
+
+                int bitCount = Integer.bitCount(val);
+                if (bitCount == currentBitCount) {
+                    segmentMin = Math.min(segmentMin, val);
+                    segmentMax = Math.max(segmentMax, val);
+                } else {
+                    // new segment
+
+                    if (segmentMin < prevSegmentMax) {
+                        return false;
+                    }
+
+                    prevSegmentMax = segmentMax;
+
+                    currentBitCount = bitCount;
+                    segmentMin = val;
+                    segmentMax = val;
+                }
+            }
+
+            if (segmentMin < prevSegmentMax) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+// [8,4,2,30,15]
+// [3,16,8,4,2]
+}
+
+//    3011. Find if Array Can Be Sorted
+//    Medium
+//    You are given a 0-indexed array of positive integers nums.
+//
+//    In one operation, you can swap any two adjacent elements if they have the same number of set bits. You are allowed to do this operation any number of times (including zero).
+//
+//    Return true if you can sort the array, else return false.
+//
+//
+//
+//    Example 1:
+//
+//    Input: nums = [8,4,2,30,15]
+//    Output: true
+//    Explanation: Let's look at the binary representation of every element. The numbers 2, 4, and 8 have one set bit each with binary representation "10", "100", and "1000" respectively. The numbers 15 and 30 have four set bits each with binary representation "1111" and "11110".
+//    We can sort the array using 4 operations:
+//    - Swap nums[0] with nums[1]. This operation is valid because 8 and 4 have one set bit each. The array becomes [4,8,2,30,15].
+//    - Swap nums[1] with nums[2]. This operation is valid because 8 and 2 have one set bit each. The array becomes [4,2,8,30,15].
+//    - Swap nums[0] with nums[1]. This operation is valid because 4 and 2 have one set bit each. The array becomes [2,4,8,30,15].
+//    - Swap nums[3] with nums[4]. This operation is valid because 30 and 15 have four set bits each. The array becomes [2,4,8,15,30].
+//    The array has become sorted, hence we return true.
+//    Note that there may be other sequences of operations which also sort the array.
+//    Example 2:
+//
+//    Input: nums = [1,2,3,4,5]
+//    Output: true
+//    Explanation: The array is already sorted, hence we return true.
+//    Example 3:
+//
+//    Input: nums = [3,16,8,4,2]
+//    Output: false
+//    Explanation: It can be shown that it is not possible to sort the input array using any number of operations.
+//
+//
+//    Constraints:
+//
+//    1 <= nums.length <= 100
+//    1 <= nums[i] <= 28
