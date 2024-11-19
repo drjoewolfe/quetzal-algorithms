3d45fcae5329781ab4816710185ed5ba1a92179f
Appu Thampi <drjoewolfe@gmail.com>
2024-11-19 19:16:58 +0530

    2461. Maximum Sum of Distinct Subarrays With Length K

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/MaximumSumOfDistinctSubarraysWithLengthK.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MaximumSumOfDistinctSubarraysWithLengthK.java
@@ -8,32 +8,70 @@ import java.util.Set;
 public class MaximumSumOfDistinctSubarraysWithLengthK {
     class Solution {
         public long maximumSubarraySum(int[] nums, int k) {
-            if(nums == null || nums.length == 0 || nums.length < k) {
+            if (nums == null || nums.length == 0 || nums.length < k) {
+                return 0;
+            }
+
+            int n = nums.length;
+            int left = 0;
+            int right = 0;
+
+            long maxSum = 0;
+            long sum = 0;
+            Map<Integer, Integer> numToIndex = new HashMap<>();
+
+            while (right < n) {
+                int val = nums[right];
+                int lastOccurance = numToIndex.getOrDefault(val, -1);
+
+                while (left <= lastOccurance
+                        || right - left + 1 > k) {
+                    sum -= nums[left];
+                    left++;
+                }
+
+                numToIndex.put(val, right);
+                sum += val;
+
+                if (right - left + 1 == k) {
+                    maxSum = Math.max(maxSum, sum);
+                }
+
+                right++;
+            }
+
+            return maxSum;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public long maximumSubarraySum(int[] nums, int k) {
+            if (nums == null || nums.length == 0 || nums.length < k) {
                 return 0;
             }
 
             long maxSum = 0;
             long sum = 0;
             Map<Integer, Integer> frequencies = new HashMap<>();
-            for(int i = 0; i < k - 1; i++) {
+            for (int i = 0; i < k - 1; i++) {
                 int a = nums[i];
                 sum += a;
 
                 frequencies.put(a, frequencies.getOrDefault(a, 0) + 1);
             }
 
-            for(int i = k - 1; i < nums.length; i++) {
+            for (int i = k - 1; i < nums.length; i++) {
                 int a = nums[i];
                 sum += a;
                 frequencies.put(a, frequencies.getOrDefault(a, 0) + 1);
 
-                if(frequencies.size() == k) {
+                if (frequencies.size() == k) {
                     maxSum = Math.max(maxSum, sum);
                 }
 
                 int b = nums[i - k + 1];
                 sum -= b;
-                if(frequencies.get(b) == 1) {
+                if (frequencies.get(b) == 1) {
                     frequencies.remove(b);
                 } else {
                     frequencies.put(b, frequencies.get(b) - 1);
@@ -46,19 +84,19 @@ public class MaximumSumOfDistinctSubarraysWithLengthK {
 
     class Solution_Brute {
         public long maximumSubarraySum(int[] nums, int k) {
-            if(nums == null || nums.length == 0 || nums.length < k) {
+            if (nums == null || nums.length == 0 || nums.length < k) {
                 return 0;
             }
 
             long maxSum = 0;
             int left = 0;
-            for(int right = k - 1; right < nums.length; right++) {
+            for (int right = k - 1; right < nums.length; right++) {
                 boolean valid = true;
                 long sum = 0;
                 Set<Integer> set = new HashSet<>();
-                for(int i = left; i <= right; i++) {
+                for (int i = left; i <= right; i++) {
                     int a = nums[i];
-                    if(set.contains(a)) {
+                    if (set.contains(a)) {
                         valid = false;
                         break;
                     }
@@ -67,7 +105,7 @@ public class MaximumSumOfDistinctSubarraysWithLengthK {
                     sum += a;
                 }
 
-                if(valid) {
+                if (valid) {
                     maxSum = Math.max(maxSum, sum);
                 }
 
