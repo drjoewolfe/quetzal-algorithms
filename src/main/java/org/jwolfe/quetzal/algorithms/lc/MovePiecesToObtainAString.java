1a71f73afeb91b7155d5678434eca4eec93e6097
Appu Thampi <drjoewolfe@gmail.com>
2024-12-05 08:51:18 +0530

    2337. Move Pieces to Obtain a String

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/MovePiecesToObtainAString.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MovePiecesToObtainAString.java
@@ -1,9 +1,66 @@
 package org.jwolfe.quetzal.algorithms.lc;
 
+import org.jwolfe.quetzal.library.general.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MovePiecesToObtainAString {
     class Solution {
         public boolean canChange(String start, String target) {
-            if(start == null || target == null || start.length() != target.length()) {
+            if ((start == null && target == null) || (start.length() == 0 && target.length() == 0)) {
+                return true;
+            }
+
+            if (start == null || target == null || start.length() != target.length()) {
+                return false;
+            }
+
+            Queue<Pair<Character, Integer>> startQueue = new LinkedList<>();
+            Queue<Pair<Character, Integer>> targetQueue = new LinkedList<>();
+
+            int n = start.length();
+            for (int i = 0; i < n; i++) {
+                char sc = start.charAt(i);
+                char tc = target.charAt(i);
+
+                if (sc != '_') {
+                    startQueue.offer(new Pair<>(sc, i));
+                }
+
+                if (tc != '_') {
+                    targetQueue.offer(new Pair<>(tc, i));
+                }
+            }
+
+            if (startQueue.size() != targetQueue.size()) {
+                return false;
+            }
+
+            while (!startQueue.isEmpty()) {
+                var startElement = startQueue.poll();
+                var targetElement = targetQueue.poll();
+
+                char sc = startElement.getKey();
+                char tc = targetElement.getKey();
+
+                int si = startElement.getValue();
+                int ti = targetElement.getValue();
+
+                if (sc != tc
+                        || (sc == 'L' && si < ti)
+                        || (sc == 'R' && si > ti)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public boolean canChange(String start, String target) {
+            if (start == null || target == null || start.length() != target.length()) {
                 return false;
             }
 
@@ -11,36 +68,36 @@ public class MovePiecesToObtainAString {
             int si = 0;
             int ti = 0;
 
-            while(si < n || ti < n) {
-                while(si < n && start.charAt(si) == '_') {
+            while (si < n || ti < n) {
+                while (si < n && start.charAt(si) == '_') {
                     si++;
                 }
 
-                while(ti < n && target.charAt(ti) == '_') {
+                while (ti < n && target.charAt(ti) == '_') {
                     ti++;
                 }
 
-                if(si == n && ti == n) {
+                if (si == n && ti == n) {
                     return true;
                 }
 
-                if(si == n || ti == n) {
+                if (si == n || ti == n) {
                     return false;
                 }
 
                 char sc = start.charAt(si);
                 char tc = target.charAt(ti);
 
-                if(sc != tc) {
+                if (sc != tc) {
                     return false;
                 }
 
-                if(sc == 'L') {
-                    if(si < ti) {
+                if (sc == 'L') {
+                    if (si < ti) {
                         return false;
                     }
                 } else {
-                    if(si > ti) {
+                    if (si > ti) {
                         return false;
                     }
                 }
