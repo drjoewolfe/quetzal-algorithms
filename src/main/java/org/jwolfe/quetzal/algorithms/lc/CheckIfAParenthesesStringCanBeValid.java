34c0436ab8dd469a9f5a68bf258a03be0d63b9be
Appu Thampi <drjoewolfe@gmail.com>
2025-01-13 01:17:32 +0530

    2116. Check if a Parentheses String Can Be Valid

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CheckIfAParenthesesStringCanBeValid.java
@@ -0,0 +1,93 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.Stack;
+
+public class CheckIfAParenthesesStringCanBeValid {
+    class Solution {
+        public boolean canBeValid(String s, String locked) {
+            if (s == null || s.length() == 0) {
+                return true;
+            }
+
+            int n = s.length();
+            if (n % 2 == 1) {
+                return false;
+            }
+
+            Stack<Integer> lockedOpenBrackets = new Stack<>();
+            Stack<Integer> unlockedIndices = new Stack<>();
+
+            for (int i = 0; i < n; i++) {
+                char c = s.charAt(i);
+                char l = locked.charAt(i);
+
+                if (l == '0') {
+                    unlockedIndices.push(i);
+                } else if (c == '(') {
+                    // Locked - Open
+                    lockedOpenBrackets.push(i);
+                } else if (c == ')') {
+                    // Locked - Close
+                    if (!lockedOpenBrackets.isEmpty()) {
+                        lockedOpenBrackets.pop();
+                    } else if (!unlockedIndices.isEmpty()) {
+                        unlockedIndices.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+
+            while (!lockedOpenBrackets.isEmpty()
+                    && !unlockedIndices.isEmpty()
+                    && lockedOpenBrackets.peek() < unlockedIndices.peek()) {
+                lockedOpenBrackets.pop();
+                unlockedIndices.pop();
+            }
+
+            return lockedOpenBrackets.isEmpty();
+        }
+    }
+}
+
+//    2116. Check if a Parentheses String Can Be Valid
+//    Medium
+//    A parentheses string is a non-empty string consisting only of '(' and ')'. It is valid if any of the following conditions is true:
+//
+//    It is ().
+//    It can be written as AB (A concatenated with B), where A and B are valid parentheses strings.
+//    It can be written as (A), where A is a valid parentheses string.
+//    You are given a parentheses string s and a string locked, both of length n. locked is a binary string consisting only of '0's and '1's. For each index i of locked,
+//
+//    If locked[i] is '1', you cannot change s[i].
+//    But if locked[i] is '0', you can change s[i] to either '(' or ')'.
+//    Return true if you can make s a valid parentheses string. Otherwise, return false.
+//
+//
+//
+//    Example 1:
+//
+//
+//    Input: s = "))()))", locked = "010100"
+//    Output: true
+//    Explanation: locked[1] == '1' and locked[3] == '1', so we cannot change s[1] or s[3].
+//    We change s[0] and s[4] to '(' while leaving s[2] and s[5] unchanged to make s valid.
+//    Example 2:
+//
+//    Input: s = "()()", locked = "0000"
+//    Output: true
+//    Explanation: We do not need to make any changes because s is already valid.
+//    Example 3:
+//
+//    Input: s = ")", locked = "0"
+//    Output: false
+//    Explanation: locked permits us to change s[0].
+//    Changing s[0] to either '(' or ')' will not make s valid.
+//
+//
+//    Constraints:
+//
+//    n == s.length == locked.length
+//    1 <= n <= 105
+//    s[i] is either '(' or ')'.
+//    locked[i] is either '0' or '1'.
