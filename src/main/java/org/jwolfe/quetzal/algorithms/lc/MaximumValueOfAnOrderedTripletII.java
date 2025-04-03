ef24947cadfa6e2006a4703da132ad9d3cf6116d
Appu Thampi <drjoewolfe@gmail.com>
2025-04-04 03:44:00 +0530

    2874. Maximum Value of an Ordered Triplet II

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MaximumValueOfAnOrderedTripletII.java
@@ -0,0 +1,56 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class MaximumValueOfAnOrderedTripletII {
+    class Solution {
+        public long maximumTripletValue(int[] nums) {
+            if (nums == null || nums.length < 3) {
+                return 0;
+            }
+
+            long maxValue = 0;
+            int maxFirst = 0;
+            int maxDiff = 0;
+            for (int val : nums) {
+                maxValue = Math.max(maxValue, 1L * maxDiff * val);
+                maxDiff = Math.max(maxDiff, maxFirst - val);
+                maxFirst = Math.max(maxFirst, val);
+            }
+
+            return maxValue;
+        }
+    }
+}
+
+//    2874. Maximum Value of an Ordered Triplet II
+//    Medium
+//    You are given a 0-indexed integer array nums.
+//
+//    Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+//
+//    The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+//
+//
+//
+//    Example 1:
+//
+//    Input: nums = [12,6,1,2,7]
+//    Output: 77
+//    Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+//    It can be shown that there are no ordered triplets of indices with a value greater than 77.
+//    Example 2:
+//
+//    Input: nums = [1,10,3,4,19]
+//    Output: 133
+//    Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+//    It can be shown that there are no ordered triplets of indices with a value greater than 133.
+//    Example 3:
+//
+//    Input: nums = [1,2,3]
+//    Output: 0
+//    Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+//
+//
+//    Constraints:
+//
+//    3 <= nums.length <= 105
+//    1 <= nums[i] <= 106
