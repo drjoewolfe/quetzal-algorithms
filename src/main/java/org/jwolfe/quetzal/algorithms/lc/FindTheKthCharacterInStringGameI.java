5b5bddaf7341fbc87d7dac0157680ecec48885d6
Appu Thampi <drjoewolfe@gmail.com>
2025-07-04 18:41:24 +0530

    3304. Find the K-th Character in String Game I

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/FindTheKthCharacterInStringGameI.java
@@ -0,0 +1,64 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class FindTheKthCharacterInStringGameI {
+    class Solution {
+        public char kthCharacter(int k) {
+            int shifts = 0;
+            int t;
+
+            while (k != 1) {
+                t = 31 - Integer.numberOfLeadingZeros(k);
+                if ((1 << t) == k) {
+                    t--;
+                }
+
+                k = k - (1 << t);
+                shifts++;
+            }
+
+            return (char) ('a' + shifts);
+        }
+    }
+}
+
+//    3304. Find the K-th Character in String Game I
+//    Easy
+//    Alice and Bob are playing a game. Initially, Alice has a string word = "a".
+//
+//    You are given a positive integer k.
+//
+//    Now Bob will ask Alice to perform the following operation forever:
+//
+//    Generate a new string by changing each character in word to its next character in the English alphabet, and append it to the original word.
+//    For example, performing the operation on "c" generates "cd" and performing the operation on "zb" generates "zbac".
+//
+//    Return the value of the kth character in word, after enough operations have been done for word to have at least k characters.
+//
+//    Note that the character 'z' can be changed to 'a' in the operation.
+//
+//
+//
+//    Example 1:
+//
+//    Input: k = 5
+//
+//    Output: "b"
+//
+//    Explanation:
+//
+//    Initially, word = "a". We need to do the operation three times:
+//
+//    Generated string is "b", word becomes "ab".
+//    Generated string is "bc", word becomes "abbc".
+//    Generated string is "bccd", word becomes "abbcbccd".
+//    Example 2:
+//
+//    Input: k = 10
+//
+//    Output: "c"
+//
+//
+//
+//    Constraints:
+//
+//    1 <= k <= 500
