26edfa5bac4c5fb5197f7372f8614a461cb5b54e
Appu Thampi <drjoewolfe@gmail.com>
2025-08-11 03:14:57 +0530

    v

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/ReorderedPowerOf2.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/ReorderedPowerOf2.java
@@ -6,15 +6,56 @@ import java.util.List;
 
 public class ReorderedPowerOf2 {
     class Solution {
+        public boolean reorderedPowerOf2(int n) {
+            if (n <= 0) {
+                return false;
+            }
+
+            int[] targetDigitFrequencies = getDigitFrequencies(n);
+            for (int i = 0; i < 32; i++) {
+                int power = (1 << i);
+                int[] powerDigitFrequencies = getDigitFrequencies(power);
+                if (areEqual(targetDigitFrequencies, powerDigitFrequencies)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private int[] getDigitFrequencies(int n) {
+            int[] frequencies = new int[10];
+            while (n > 0) {
+                int digit = n % 10;
+                frequencies[digit]++;
+
+                n /= 10;
+            }
+
+            return frequencies;
+        }
+
+        private boolean areEqual(int[] arr1, int[] arr2) {
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i] != arr2[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class Solution_Correct_1 {
         public boolean reorderedPowerOf2(int N) {
-            if(N <= 0) {
+            if (N <= 0) {
                 return false;
             }
 
             int[] digitCounts = getDigitCounts(N);
-            for(int i = 0; i < 32; i++) {
+            for (int i = 0; i < 32; i++) {
                 int[] powerOfTwoDigitCount = getDigitCounts(1 << i);
-                if(Arrays.equals(digitCounts, powerOfTwoDigitCount)) {
+                if (Arrays.equals(digitCounts, powerOfTwoDigitCount)) {
                     return true;
                 }
             }
@@ -24,7 +65,7 @@ public class ReorderedPowerOf2 {
 
         private int[] getDigitCounts(int n) {
             int[] digitCounts = new int[10];
-            while(n > 0) {
+            while (n > 0) {
                 digitCounts[n % 10]++;
                 n /= 10;
             }
@@ -35,7 +76,7 @@ public class ReorderedPowerOf2 {
 
     class Solution_Permutation {
         public boolean reorderedPowerOf2(int N) {
-            if(N <= 0) {
+            if (N <= 0) {
                 return false;
             }
 
@@ -44,19 +85,19 @@ public class ReorderedPowerOf2 {
         }
 
         private boolean permuteForPowerOf2(List<Integer> digits, int index) {
-            if(index == digits.size()) {
+            if (index == digits.size()) {
                 return isPowerOf2(digits);
             }
 
             int a = digits.get(index);
-            for(int i = index; i < digits.size(); i++) {
+            for (int i = index; i < digits.size(); i++) {
                 int b = digits.get(i);
-                if(index == 0 && b == 0) {
+                if (index == 0 && b == 0) {
                     continue;
                 }
 
                 swap(digits, index, i);
-                if(permuteForPowerOf2(digits, index + 1)) {
+                if (permuteForPowerOf2(digits, index + 1)) {
                     return true;
                 }
 
@@ -69,7 +110,7 @@ public class ReorderedPowerOf2 {
         private List<Integer> getDigits(int N) {
             List<Integer> digits = new ArrayList<>();
 
-            while(N > 0) {
+            while (N > 0) {
                 digits.add(N % 10);
                 N /= 10;
             }
@@ -85,13 +126,13 @@ public class ReorderedPowerOf2 {
 
         private boolean isPowerOf2(List<Integer> digits) {
             int x = 0;
-            for(int i = 0; i < digits.size(); i++) {
+            for (int i = 0; i < digits.size(); i++) {
                 x *= 10;
                 x += digits.get(i);
             }
 
-            while(x > 1) {
-                if(x % 2 != 0) {
+            while (x > 1) {
+                if (x % 2 != 0) {
                     return false;
                 }
 
