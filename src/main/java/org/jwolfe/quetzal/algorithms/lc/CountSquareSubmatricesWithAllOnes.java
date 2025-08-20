7b844ef1951f73c0e31413d9d7ab218e9840b586
Appu Thampi <drjoewolfe@gmail.com>
2025-08-20 18:49:34 +0530

    1277. Count Square Submatrices with All Ones

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/CountSquareSubmatricesWithAllOnes.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/CountSquareSubmatricesWithAllOnes.java
@@ -11,6 +11,32 @@ public class CountSquareSubmatricesWithAllOnes {
             int n = matrix[0].length;
 
             int count = 0;
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == 0) {
+                        continue;
+                    }
+
+                    dp[i + 1][j + 1] = Math.min(dp[i][j], Math.min(dp[i + 1][j], dp[i][j + 1])) + 1;
+                    count += dp[i + 1][j + 1];
+                }
+            }
+
+            return count;
+        }
+    }
+
+    class Solution_Correct_2 {
+        public int countSquares(int[][] matrix) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return 0;
+            }
+
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            int count = 0;
             int[][] dp = new int[m][n];
 
             for (int i = 0; i < m; i++) {
