2f659984c6605dbcf5c07e854ceb15d7391e2719
Appu Thampi <drjoewolfe@gmail.com>
2025-09-15 02:44:00 +0530

    966. Vowel Spellchecker

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/VowelSpellchecker.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/VowelSpellchecker.java
@@ -8,7 +8,74 @@ import java.util.Set;
 public class VowelSpellchecker {
     class Solution {
         public String[] spellchecker(String[] wordlist, String[] queries) {
-            if(wordlist == null || wordlist.length == 0 || queries == null || queries.length == 0) {
+            if (wordlist == null || wordlist.length == 0 || queries == null || queries.length == 0) {
+                return new String[0];
+            }
+
+            Set<String> exactWords = new HashSet<>();
+            Map<String, String> capitalizedMap = new HashMap<>();
+            Map<String, String> deVoweledMap = new HashMap<>();
+
+            for (String word : wordlist) {
+                exactWords.add(word);
+
+                String lowerCaseWord = word.toLowerCase();
+                capitalizedMap.putIfAbsent(lowerCaseWord, word);
+
+                String deVoweledWord = deVowel(lowerCaseWord);
+                deVoweledMap.putIfAbsent(deVoweledWord, word);
+            }
+
+            int n = queries.length;
+            String[] answer = new String[n];
+            for (int i = 0; i < n; i++) {
+                String query = queries[i];
+                answer[i] = solve(query, exactWords, capitalizedMap, deVoweledMap);
+            }
+
+            return answer;
+        }
+
+        private String solve(String query, Set<String> exactWords, Map<String, String> capitalizedMap, Map<String, String> deVoweledMap) {
+            if (exactWords.contains(query)) {
+                return query;
+            }
+
+            String lowerCaseQuery = query.toLowerCase();
+            if (capitalizedMap.containsKey(lowerCaseQuery)) {
+                return capitalizedMap.get(lowerCaseQuery);
+            }
+
+            String deVoweledQuery = deVowel(lowerCaseQuery);
+            if (deVoweledMap.containsKey(deVoweledQuery)) {
+                return deVoweledMap.get(deVoweledQuery);
+            }
+
+            return "";
+        }
+
+        private String deVowel(String word) {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (isVowel(c)) {
+                    builder.append("*");
+                } else {
+                    builder.append(c);
+                }
+            }
+
+            return builder.toString();
+        }
+
+        private boolean isVowel(char c) {
+            return (c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u');
+        }
+    }
+
+    class Solution_Correct_1 {
+        public String[] spellchecker(String[] wordlist, String[] queries) {
+            if (wordlist == null || wordlist.length == 0 || queries == null || queries.length == 0) {
                 return new String[0];
             }
 
@@ -16,38 +83,38 @@ public class VowelSpellchecker {
             Map<String, String> lowerCasedMap = new HashMap<>();
             Map<String, String> vowelMaskedMap = new HashMap<>();
 
-            for(String word : wordlist) {
+            for (String word : wordlist) {
                 wordSet.add(word);
 
                 String lowerCasedWord = word.toLowerCase();
-                if(!lowerCasedMap.containsKey(lowerCasedWord)) {
+                if (!lowerCasedMap.containsKey(lowerCasedWord)) {
                     lowerCasedMap.put(lowerCasedWord, word);
                 }
 
                 String vowelMaskedWord = getVowelMaskedWord(lowerCasedWord);
-                if(!vowelMaskedMap.containsKey(vowelMaskedWord)) {
+                if (!vowelMaskedMap.containsKey(vowelMaskedWord)) {
                     vowelMaskedMap.put(vowelMaskedWord, word);
                 }
             }
 
             String[] output = new String[queries.length];
-            for(int i = 0; i < queries.length; i++) {
+            for (int i = 0; i < queries.length; i++) {
                 String query = queries[i];
-                if(wordSet.contains(query)) {
+                if (wordSet.contains(query)) {
                     output[i] = query;
                 } else {
                     String lowerCasedQuery = query.toLowerCase();
-                    if(lowerCasedMap.containsKey(lowerCasedQuery)) {
+                    if (lowerCasedMap.containsKey(lowerCasedQuery)) {
                         output[i] = lowerCasedMap.get(lowerCasedQuery);
                     } else {
                         String vowelMaskedWord = getVowelMaskedWord(lowerCasedQuery);
-                        if(vowelMaskedMap.containsKey(vowelMaskedWord)) {
+                        if (vowelMaskedMap.containsKey(vowelMaskedWord)) {
                             output[i] = vowelMaskedMap.get(vowelMaskedWord);
                         }
                     }
                 }
 
-                if(output[i] == null) {
+                if (output[i] == null) {
                     output[i] = "";
                 }
             }
@@ -57,8 +124,8 @@ public class VowelSpellchecker {
 
         private String getVowelMaskedWord(String word) {
             StringBuilder builder = new StringBuilder(word);
-            for(int i = 0; i < builder.length(); i++) {
-                if(isVowel(builder.charAt(i))) {
+            for (int i = 0; i < builder.length(); i++) {
+                if (isVowel(builder.charAt(i))) {
                     builder.setCharAt(i, '*');
                 }
             }
