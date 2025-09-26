983c594e1ddd04cb19f591c8e0e2b6f4aac1feee
Appu Thampi <drjoewolfe@gmail.com>
2025-09-26 17:17:13 +0530

    611. Valid Triangle Number

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/ValidTriangleNumber.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/ValidTriangleNumber.java
@@ -5,20 +5,63 @@ import java.util.Arrays;
 public class ValidTriangleNumber {
     class Solution {
         public int triangleNumber(int[] nums) {
-            if(nums == null || nums.length < 3) {
+            if (nums == null || nums.length < 0) {
+                return 0;
+            }
+
+            Arrays.sort(nums);
+
+            int count = 0;
+            int n = nums.length;
+
+            for (int i = 0; i < n - 2; i++) {
+                int a = nums[i];
+
+                for (int j = i + 1; j < n - 1; j++) {
+                    int b = nums[j];
+
+                    int k = binarySearchForGreaterOrEqual(nums, j + 1, n - 1, a + b);
+                    if (k > j) {
+                        count += (k - j - 1);
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private int binarySearchForGreaterOrEqual(int[] nums, int left, int right, int target) {
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                int val = nums[mid];
+
+                if (val >= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public int triangleNumber(int[] nums) {
+            if (nums == null || nums.length < 3) {
                 return 0;
             }
 
             Arrays.sort(nums);
             int count = 0;
             int n = nums.length;
-            for(int i = 0; i < n - 2; i++) {
+            for (int i = 0; i < n - 2; i++) {
                 int a = nums[i];
-                for(int j = i + 1; j < n - 1; j++) {
+                for (int j = i + 1; j < n - 1; j++) {
                     int b = nums[j];
                     int k = binarySearchForGreaterOrEqual(nums, j + 1, n - 1, a + b);
 
-                    if(k > 0) {
+                    if (k > 0) {
                         count += (k - j - 1);
                     }
                 }
@@ -28,10 +71,10 @@ public class ValidTriangleNumber {
         }
 
         private int binarySearchForGreaterOrEqual(int[] nums, int left, int right, int x) {
-            while(left <= right) {
+            while (left <= right) {
                 int mid = left + (right - left) / 2;
 
-                if(nums[mid] >= x) {
+                if (nums[mid] >= x) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
@@ -44,20 +87,20 @@ public class ValidTriangleNumber {
 
     class Solution_Brute {
         public int triangleNumber(int[] nums) {
-            if(nums == null || nums.length < 3) {
+            if (nums == null || nums.length < 3) {
                 return 0;
             }
 
             int count = 0;
             int n = nums.length;
-            for(int i = 0; i < n - 2; i++) {
+            for (int i = 0; i < n - 2; i++) {
                 int a = nums[i];
-                for(int j = i + 1; j < n - 1; j++) {
+                for (int j = i + 1; j < n - 1; j++) {
                     int b = nums[j];
-                    for(int k = j + 1; k < n; k++) {
+                    for (int k = j + 1; k < n; k++) {
                         int c = nums[k];
 
-                        if(isValidTriangle(a, b, c)) {
+                        if (isValidTriangle(a, b, c)) {
                             count++;
                         }
                     }
