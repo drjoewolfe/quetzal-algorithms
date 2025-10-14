0d153211bc8b2ea0ed54e4514a84cbf226e1f6fc
Appu Thampi <drjoewolfe@gmail.com>
2025-10-15 02:27:28 +0530

    3349. Adjacent Increasing Subarrays Detection I

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/AdjacentIncreasingSubarraysDetectionI.java
@@ -0,0 +1,71 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+import java.util.List;
+
+public class AdjacentIncreasingSubarraysDetectionI {
+    class Solution {
+        public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+            if (nums == null || nums.size() < 2 || k < 1) {
+                return false;
+            }
+
+            int n = nums.size();
+            int maxPairLength = 0;
+
+            int prevArrLength = 0;
+            int currArrLength = 1;
+
+            for (int i = 1; i < n; i++) {
+                int prev = nums.get(i - 1);
+                int curr = nums.get(i);
+
+                if (curr > prev) {
+                    currArrLength++;
+                } else {
+                    prevArrLength = currArrLength;
+                    currArrLength = 1;
+                }
+
+                maxPairLength = Math.max(maxPairLength, Math.min(prevArrLength, currArrLength));
+                maxPairLength = Math.max(maxPairLength, currArrLength / 2);
+            }
+
+            return maxPairLength >= k;
+        }
+    }
+}
+
+//    3349. Adjacent Increasing Subarrays Detection I
+//    Easy
+//    Given an array nums of n integers and an integer k, determine whether there exist two adjacent subarrays of length k such that both subarrays are strictly increasing. Specifically, check if there are two subarrays starting at indices a and b (a < b), where:
+//
+//    Both subarrays nums[a..a + k - 1] and nums[b..b + k - 1] are strictly increasing.
+//    The subarrays must be adjacent, meaning b = a + k.
+//    Return true if it is possible to find two such subarrays, and false otherwise.
+//
+//
+//
+//    Example 1:
+//
+//    Input: nums = [2,5,7,8,9,2,3,4,3,1], k = 3
+//
+//    Output: true
+//
+//    Explanation:
+//
+//    The subarray starting at index 2 is [7, 8, 9], which is strictly increasing.
+//    The subarray starting at index 5 is [2, 3, 4], which is also strictly increasing.
+//    These two subarrays are adjacent, so the result is true.
+//    Example 2:
+//
+//    Input: nums = [1,2,3,4,4,4,4,5,6,7], k = 5
+//
+//    Output: false
+//
+//
+//
+//    Constraints:
+//
+//    2 <= nums.length <= 100
+//    1 < 2 * k <= nums.length
+//    -1000 <= nums[i] <= 1000
