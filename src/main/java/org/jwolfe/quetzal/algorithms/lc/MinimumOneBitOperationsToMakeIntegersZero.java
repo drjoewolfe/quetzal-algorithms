602102432a64cad8eb8beb136b65eb11bdb74f19
Appu Thampi <drjoewolfe@gmail.com>
2025-11-08 18:53:01 +0530

    1611. Minimum One Bit Operations to Make Integers Zero

---
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumOneBitOperationsToMakeIntegersZero.java
@@ -0,0 +1,53 @@
+package org.jwolfe.quetzal.algorithms.lc;
+
+public class MinimumOneBitOperationsToMakeIntegersZero {
+    class Solution {
+        public int minimumOneBitOperations(int n) {
+            if (n == 0) {
+                return 0;
+            }
+
+            // Get most significant bit
+            int k = 0;
+            int curr = 1;
+            while (curr * 2 <= n) {
+                k += 1;
+                curr *= 2;
+            }
+
+            return (1 << (k + 1)) - 1 - minimumOneBitOperations(n ^ curr);
+        }
+    }
+}
+
+//    1611. Minimum One Bit Operations to Make Integers Zero
+//    Hard
+//    Given an integer n, you must transform it into 0 using the following operations any number of times:
+//
+//    Change the rightmost (0th) bit in the binary representation of n.
+//    Change the ith bit in the binary representation of n if the (i-1)th bit is set to 1 and the (i-2)th through 0th bits are set to 0.
+//    Return the minimum number of operations to transform n into 0.
+//
+//
+//
+//    Example 1:
+//
+//    Input: n = 3
+//    Output: 2
+//    Explanation: The binary representation of 3 is "11".
+//    "11" -> "01" with the 2nd operation since the 0th bit is 1.
+//    "01" -> "00" with the 1st operation.
+//    Example 2:
+//
+//    Input: n = 6
+//    Output: 4
+//    Explanation: The binary representation of 6 is "110".
+//    "110" -> "010" with the 2nd operation since the 1st bit is 1 and 0th through 0th bits are 0.
+//    "010" -> "011" with the 1st operation.
+//    "011" -> "001" with the 2nd operation since the 0th bit is 1.
+//    "001" -> "000" with the 1st operation.
+//
+//
+//    Constraints:
+//
+//    0 <= n <= 109
