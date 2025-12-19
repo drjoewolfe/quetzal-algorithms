2b8e85736c37afbfd59a910281bdb2cb6781b85e
Appu Thampi <drjoewolfe@gmail.com>
2025-12-20 01:10:09 +0530

    2092. Find All People With Secret

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/FindAllPeopleWithSecret.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/FindAllPeopleWithSecret.java
@@ -7,17 +7,119 @@ import java.util.*;
 public class FindAllPeopleWithSecret {
     class Solution {
         public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+            List<Integer> results = new ArrayList<>();
+
+            if (n < 1) {
+                return results;
+            }
+
+            Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
+
+            TreeMap<Integer, List<int[]>> meetingsByTime = new TreeMap<>();
+            for (int[] meeting : meetings) {
+                int a = meeting[0];
+                int b = meeting[1];
+                int t = meeting[2];
+
+                if (!meetingsByTime.containsKey(t)) {
+                    meetingsByTime.put(t, new ArrayList<>());
+                }
+
+                meetingsByTime.get(t).add(new int[]{a, b});
+            }
+
+            DisjointSet ds = new DisjointSet(n);
+            ds.union(0, firstPerson);
+
+            for (int t : meetingsByTime.keySet()) {
+                for (int[] meeting : meetingsByTime.get(t)) {
+                    int a = meeting[0];
+                    int b = meeting[1];
+
+                    ds.union(a, b);
+                }
+
+                for (int[] meeting : meetingsByTime.get(t)) {
+                    int a = meeting[0];
+                    int b = meeting[1];
+
+                    if (!ds.isConnected(a, 0)) {
+                        ds.reset(a);
+                        ds.reset(b);
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (ds.isConnected(i, 0)) {
+                    results.add(i);
+                }
+            }
+
+            return results;
+        }
+
+        private class DisjointSet {
+            int[] parent;
+            int[] rank;
+
+            public DisjointSet(int n) {
+                parent = new int[n];
+                rank = new int[n];
+
+                for (int x = 0; x < n; x++) {
+                    parent[x] = x;
+                }
+            }
+
+            public void union(int x, int y) {
+                int px = find(x);
+                int py = find(y);
+
+                if (px != py) {
+                    if (rank[px] > rank[py]) {
+                        parent[py] = px;
+                    } else if (rank[px] < rank[py]) {
+                        parent[px] = py;
+                    } else {
+                        parent[py] = px;
+                        rank[px]++;
+                    }
+                }
+            }
+
+            public int find(int x) {
+                if (parent[x] != x) {
+                    parent[x] = find(parent[x]);
+                }
+
+                return parent[x];
+            }
+
+            public boolean isConnected(int x, int y) {
+                return find(x) == find(y);
+            }
+
+            public void reset(int x) {
+                parent[x] = x;
+                rank[x] = 0;
+            }
+        }
+    }
+
+    class Solution_Correct_1 {
+        public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
             Map<Integer, List<Pair<Integer, Integer>>> graph = new HashMap<>();
             for (int[] meeting : meetings) {
                 int x = meeting[0];
                 int y = meeting[1];
                 int t = meeting[2];
 
-                if(!graph.containsKey(x)) {
+                if (!graph.containsKey(x)) {
                     graph.put(x, new ArrayList<>());
                 }
 
-                if(!graph.containsKey(y)) {
+                if (!graph.containsKey(y)) {
                     graph.put(y, new ArrayList<>());
                 }
 
@@ -35,21 +137,21 @@ public class FindAllPeopleWithSecret {
             queue.offer(new Pair<>(0, 0));
             queue.offer(new Pair<>(firstPerson, 0));
 
-            while(!queue.isEmpty()) {
+            while (!queue.isEmpty()) {
                 var node = queue.poll();
 
                 int u = node.getKey();
                 int t = node.getValue();
 
-                if(!graph.containsKey(u)) {
+                if (!graph.containsKey(u)) {
                     continue;
                 }
 
-                for(var childNode : graph.get(u)) {
+                for (var childNode : graph.get(u)) {
                     int v = childNode.getKey();
                     int vt = childNode.getValue();
 
-                    if(vt >= t && earliest[v] > vt) {
+                    if (vt >= t && earliest[v] > vt) {
                         earliest[v] = vt;
                         queue.offer(new Pair<>(v, vt));
                     }
@@ -57,8 +159,8 @@ public class FindAllPeopleWithSecret {
             }
 
             List<Integer> peopleWithSecret = new ArrayList<>();
-            for(int i = 0; i < n; i++) {
-                if(earliest[i] != Integer.MAX_VALUE) {
+            for (int i = 0; i < n; i++) {
+                if (earliest[i] != Integer.MAX_VALUE) {
                     peopleWithSecret.add(i);
                 }
             }
@@ -70,22 +172,22 @@ public class FindAllPeopleWithSecret {
     class Solution_Incorrect_2 {
         public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
             Set<Integer> peopleWithSecret = new HashSet<>();
-            if(n < 1) {
+            if (n < 1) {
                 return new ArrayList<>(peopleWithSecret);
             }
 
             peopleWithSecret.add(0);
-            if(meetings == null || meetings.length == 0 || firstPerson < 0 || firstPerson >= n) {
+            if (meetings == null || meetings.length == 0 || firstPerson < 0 || firstPerson >= n) {
                 return new ArrayList<>(peopleWithSecret);
             }
 
             peopleWithSecret.add(firstPerson);
 
             TreeMap<Integer, DisjointSets> meetingsByTime = new TreeMap<>();
-            for(int[] meeting : meetings) {
+            for (int[] meeting : meetings) {
                 int time = meeting[2];
 
-                if(!meetingsByTime.containsKey(time)) {
+                if (!meetingsByTime.containsKey(time)) {
                     meetingsByTime.put(time, new DisjointSets());
                 }
 
@@ -96,16 +198,16 @@ public class FindAllPeopleWithSecret {
                 disjointSets.union(x, y);
             }
 
-            for(var daySchedule : meetingsByTime.entrySet()) {
+            for (var daySchedule : meetingsByTime.entrySet()) {
                 System.out.println(daySchedule.getKey());
                 var sets = daySchedule.getValue().getSets();
-                for(var set : sets) {
+                for (var set : sets) {
                     System.out.println("\t" + set);
-                    for(var person : set) {
-                        if(person.equals(5)) {
+                    for (var person : set) {
+                        if (person.equals(5)) {
                             System.out.println("DEBUG: " + set + " , " + peopleWithSecret);
                         }
-                        if(peopleWithSecret.contains(person)) {
+                        if (peopleWithSecret.contains(person)) {
                             peopleWithSecret.addAll(set);
                             break;
                         }
@@ -124,7 +226,7 @@ public class FindAllPeopleWithSecret {
             }
 
             public void add(int element) {
-                if(!parent.containsKey(element)) {
+                if (!parent.containsKey(element)) {
                     parent.put(element, -1);
                 }
             }
@@ -138,7 +240,7 @@ public class FindAllPeopleWithSecret {
             }
 
             public int find(int element) {
-                if(parent.get(element).equals(-1)) {
+                if (parent.get(element).equals(-1)) {
                     return element;
                 }
 
@@ -147,28 +249,28 @@ public class FindAllPeopleWithSecret {
 
             public List<Set> getSets() {
                 Map<Integer, Set<Integer>> setMap = new HashMap<>();
-                for(var entry : parent.entrySet()) {
+                for (var entry : parent.entrySet()) {
                     int e = entry.getKey();
                     int p = entry.getValue();
 
-                    if(p == -1) {
+                    if (p == -1) {
                         setMap.put(e, new HashSet<>());
                         setMap.get(e).add(e);
                     }
                 }
 
-                for(var entry : parent.entrySet()) {
+                for (var entry : parent.entrySet()) {
                     int e = entry.getKey();
                     int p = entry.getValue();
 
-                    if(p != -1) {
+                    if (p != -1) {
                         p = find(e);
                         setMap.get(p).add(e);
                     }
                 }
 
                 List<Set> sets = new ArrayList<>();
-                for(var entry : setMap.entrySet()) {
+                for (var entry : setMap.entrySet()) {
                     sets.add(entry.getValue());
                 }
 
@@ -180,22 +282,22 @@ public class FindAllPeopleWithSecret {
     class Solution_Incorrect {
         public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
             Set<Integer> peopleWithSecret = new HashSet<>();
-            if(n < 1) {
+            if (n < 1) {
                 return new ArrayList<>(peopleWithSecret);
             }
 
             peopleWithSecret.add(0);
-            if(meetings == null || meetings.length == 0 || firstPerson < 0 || firstPerson >= n) {
+            if (meetings == null || meetings.length == 0 || firstPerson < 0 || firstPerson >= n) {
                 return new ArrayList<>(peopleWithSecret);
             }
 
             peopleWithSecret.add(firstPerson);
 
             Map<Integer, Set<Integer>> meetingsByTime = new TreeMap<>();
-            for(int[] meeting : meetings) {
+            for (int[] meeting : meetings) {
                 int time = meeting[2];
 
-                if(!meetingsByTime.containsKey(time)) {
+                if (!meetingsByTime.containsKey(time)) {
                     meetingsByTime.put(time, new HashSet<>());
                 }
 
@@ -206,10 +308,10 @@ public class FindAllPeopleWithSecret {
                 meetingsByTime.get(time).add(y);
             }
 
-            for(var daySchedule : meetingsByTime.entrySet()) {
+            for (var daySchedule : meetingsByTime.entrySet()) {
                 var participants = daySchedule.getValue();
-                for(var person : participants) {
-                    if(peopleWithSecret.contains(person)) {
+                for (var person : participants) {
+                    if (peopleWithSecret.contains(person)) {
                         peopleWithSecret.addAll(participants);
                         break;
                     }
