811ede9ef0ce69138e9730291049e143deb81531
Appu Thampi <drjoewolfe@gmail.com>
2026-03-05 20:21:18 +0530

    1758. Minimum Changes To Make Alternating Binary String

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumChangesToMakeAlternatingBinaryString.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/MinimumChangesToMakeAlternatingBinaryString.java
@@ -3,23 +3,52 @@ package org.jwolfe.quetzal.algorithms.lc;
 public class MinimumChangesToMakeAlternatingBinaryString {
     class Solution {
         public int minOperations(String s) {
-            if(s == null || s.length() < 2) {
+            if (s == null || s.length() < 2) {
+                return 0;
+            }
+
+            // All characters at even indices should be zero
+            int start0 = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (i % 2 == 0) {
+                    // even
+                    if (c == '1') {
+                        start0++;
+                    }
+                } else {
+                    // odd
+                    if (c == '0') {
+                        start0++;
+                    }
+                }
+            }
+
+            int start1 = s.length() - start0;
+            return Math.min(start0, start1);
+        }
+    }
+
+    class Solution_Correct_2 {
+        public int minOperations(String s) {
+            if (s == null || s.length() < 2) {
                 return 0;
             }
 
             int operationsForStart0 = 0;
             int operationsForStart1 = 0;
 
-            for(int i = 0; i < s.length(); i++) {
+            for (int i = 0; i < s.length(); i++) {
                 char c = s.charAt(i);
 
-                if(i % 2 == 0) {
+                if (i % 2 == 0) {
                     // even indexes -> 0, 2, 4, 6
 
                     // For start-0, entries should be 0
                     // For start-1, entries should be 1
 
-                    if(c == '0') {
+                    if (c == '0') {
                         operationsForStart1++;
                     } else {
                         operationsForStart0++;
@@ -30,7 +59,7 @@ public class MinimumChangesToMakeAlternatingBinaryString {
                     // For start-0, entries should be 1
                     // For start-1, entries should be 0;
 
-                    if(c == '0') {
+                    if (c == '0') {
                         operationsForStart0++;
                     } else {
                         operationsForStart1++;
@@ -44,7 +73,7 @@ public class MinimumChangesToMakeAlternatingBinaryString {
 
     class Solution_Correct_1 {
         public int minOperations(String s) {
-            if(s == null || s.length() < 2) {
+            if (s == null || s.length() < 2) {
                 return 0;
             }
 
@@ -59,29 +88,29 @@ public class MinimumChangesToMakeAlternatingBinaryString {
             char option2Char = '1';
             int option2Operations = 0;
 
-            for(int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 char c = s.charAt(i);
 
-                if(c == '0') {
-                    if(option1Char == '1') {
+                if (c == '0') {
+                    if (option1Char == '1') {
                         option1Operations++;
                     }
 
-                    if(option2Char == '1') {
+                    if (option2Char == '1') {
                         option2Operations++;
                     }
                 } else {
-                    if(option1Char == '0') {
+                    if (option1Char == '0') {
                         option1Operations++;
                     }
 
-                    if(option2Char == '0') {
+                    if (option2Char == '0') {
                         option2Operations++;
                     }
                 }
 
-                option1Char = (option1Char == '0') ? '1': '0';
-                option2Char = (option2Char == '0') ? '1': '0';
+                option1Char = (option1Char == '0') ? '1' : '0';
+                option2Char = (option2Char == '0') ? '1' : '0';
             }
 
             return Math.min(option1Operations, option2Operations);
@@ -90,22 +119,22 @@ public class MinimumChangesToMakeAlternatingBinaryString {
 
     class Solution_Incorrect {
         public int minOperations(String s) {
-            if(s == null || s.length() < 2) {
+            if (s == null || s.length() < 2) {
                 return 0;
             }
 
             int n = s.length();
             boolean prevZero = s.charAt(0) == '0';
             int operations = 0;
-            for(int i = 1; i < n; i++) {
+            for (int i = 1; i < n; i++) {
                 char c = s.charAt(i);
-                if(c == '0') {
-                    if(prevZero) {
+                if (c == '0') {
+                    if (prevZero) {
                         operations++;
                         c = '1';
                     }
                 } else {
-                    if(!prevZero) {
+                    if (!prevZero) {
                         operations++;
                         c = '0';
                     }
