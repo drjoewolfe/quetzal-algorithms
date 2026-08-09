57060a18f6b0a33c9caac813442328bb40d891e5
Thampi Appu <dr.joewolfe@gmail.com>
2026-08-10 01:14:30 +0530

    1140. Stone Game II

--- src/main/java/org/jwolfe/quetzal/algorithms/lc/StoneGameII.java
+++ src/main/java/org/jwolfe/quetzal/algorithms/lc/StoneGameII.java
@@ -10,6 +10,84 @@ public class StoneGameII {
             }
 
             int n = piles.length;
+            int[] suffixSums = new int[n];
+            suffixSums[n - 1] = piles[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                suffixSums[i] = piles[i] + suffixSums[i + 1];
+            }
+
+            Integer[][] memo = new Integer[n][n];
+
+            return stoneGameII(piles, 0, 1, suffixSums, memo);
+        }
+
+        private int stoneGameII(int[] piles, int index, int m, int[] suffixSums, Integer[][] memo) {
+            int maxRange = index + 2 * m;
+            System.out.println(maxRange + ", " + index);
+            if (maxRange >= piles.length) {
+                // Can take all remaining
+                return suffixSums[index];
+            }
+
+            if (memo[index][m] != null) {
+                return memo[index][m];
+            }
+
+            int minOpponentScore = Integer.MAX_VALUE;
+            for (int x = 1; x <= 2 * m; x++) {
+                int i = index + x - 1;
+                int opponentScore = stoneGameII(piles, i + 1, Math.max(x, m), suffixSums, memo);
+                minOpponentScore = Math.min(minOpponentScore, opponentScore);
+            }
+
+            int maxScore = suffixSums[index] - minOpponentScore;
+            return memo[index][m] = maxScore;
+        }
+    }
+
+    class Solution_Recursive_TLE {
+        public int stoneGameII(int[] piles) {
+            if (piles == null || piles.length == 0) {
+                return 0;
+            }
+
+            int n = piles.length;
+            int[] suffixSums = new int[n];
+            suffixSums[n - 1] = piles[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                suffixSums[i] = piles[i] + suffixSums[i + 1];
+            }
+
+            return stoneGameII(piles, 0, 1, suffixSums);
+        }
+
+        private int stoneGameII(int[] piles, int index, int m, int[] suffixSums) {
+            int maxRange = index + 2 * m;
+            System.out.println(maxRange + ", " + index);
+            if (maxRange >= piles.length) {
+                // Can take all remaining
+                return suffixSums[index];
+            }
+
+            int minOpponentScore = Integer.MAX_VALUE;
+            for (int x = 1; x <= 2 * m; x++) {
+                int i = index + x - 1;
+                int opponentScore = stoneGameII(piles, i + 1, Math.max(x, m), suffixSums);
+                minOpponentScore = Math.min(minOpponentScore, opponentScore);
+            }
+
+            int maxScore = suffixSums[index] - minOpponentScore;
+            return maxScore;
+        }
+    }
+
+    class Solution_Correct_1 {
+        public int stoneGameII(int[] piles) {
+            if (piles == null || piles.length == 0) {
+                return 0;
+            }
+
+            int n = piles.length;
             int[] suffixSums = Arrays.copyOf(piles, n);
             for (int i = n - 2; i >= 0; i--) {
                 suffixSums[i] += suffixSums[i + 1];
